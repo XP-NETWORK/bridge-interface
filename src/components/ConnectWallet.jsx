52d966ff1e4ad198db531d82570036f35570054a
yurdaD <yurad@xp.network>
2021-11-21 18:19:32 +0200

    Continue bridge button active/dont

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -14,10 +14,13 @@ import Elrond from '../assets/img/wallet/Elrond.svg';
 import Ledger from '../assets/img/wallet/Ledger.svg';
 import Maiar from '../assets/img/wallet/Maiar.svg';
 import Trezor from '../assets/img/wallet/Trezor.svg';
+import WalletConnect from "../assets/img/wallet/WalletConnect 3.svg"
 import NFTworng from './NFTworng';
+import { useSelector } from 'react-redux';
 
 function ConnectWallet() {
-
+    const from = useSelector(state => state.general.from)
+    const to = useSelector(state => state.general.to)
     const [show, setShow] = useState(false);
     const OFF = { opacity: 0.6, pointerEvents: "none" };
     const handleClose = () => setShow(false);
@@ -25,13 +28,12 @@ function ConnectWallet() {
 
     return (
         <div>
-
             {/* <Button variant="primary" onClick={handleShow}>
                 Wallet
             </Button> */}
-            <div className="connectNft">
+            <div style={ from && to ? {} : OFF} className="connectNft">
                     <a href="#" className="themBtn" onClick={handleShow}>Continue bridging -<span>{'>'}</span> </a>
-                </div>
+            </div>
             <Modal show={show} onHide={handleClose} className="ChainModal">
                 <Modal.Header>
                     <Modal.Title>Connect Wallet</Modal.Title>
@@ -43,12 +45,12 @@ function ConnectWallet() {
                     <div className="walletListBox">
                         <ul className="walletList scrollSty">
                             <NFTworng/>
-                            <li className="wllListItem"><img src={MetaMask} /> MetaMAsk</li>
+                            <li className="wllListItem"><img src={MetaMask} /> MetaMask</li>
                             <li className="wllListItem"><img src={Elrond} /> Elrond</li>
                             <li className="wllListItem"><img src={Ledger} /> Ledger</li>
                             <li className="wllListItem"><img src={Maiar} /> Maiar</li>
                             <li className="wllListItem"><img src={Trezor} /> Trezor</li>
-                            <li className="wllListItem"><img src={"#"} /> WalletConnect</li>
+                            <li className="wllListItem"><img src={WalletConnect} /> WalletConnect</li>
                         </ul>
                     </div>
                 </Modal.Body>
