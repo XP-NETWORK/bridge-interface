f1c890e94cdbedbdd04908c0af1b87595a01ff3a
yurdaD <yurad@xp.network>
2021-11-22 16:13:04 +0200

    no nfts in list

--- src/components/innercomponents/NFTgridView.jsx
+++ src/components/innercomponents/NFTgridView.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import Algorand from '../../assets/img/chain/Algarand.svg';
 import BSC from '../../assets/img/chain/Binance.svg';
@@ -28,20 +28,26 @@ import RedClose from '../../assets/img/icons/RedClose.svg';
 import NFTdetails from '../NFTdetails';
 import { useSelector, useDispatch } from 'react-redux';
 import { setSelectedNFTList }  from "../../store/reducers/generalSlice"
+import NFTempty from './NFTempty';
 
 function NFTgridView() {
     const nfts = useSelector(state => state.general.NFTList)
+    const selectedNFTs = useSelector(state => state.general.selectedNFTList)
     const dispatch = useDispatch()
 
     const addToSelectedList = nft => {
         dispatch(setSelectedNFTList(nft))
     }
 
+
+
+    useEffect(() => { }, [selectedNFTs])
+
     return (
         <div className="nftListBox">
             <div className="row">
-                { nfts ? nfts.map( (nft, i) => <div className="col-lg-4 col-md-4 col-sm-6 col-6">
-                    <div onClick={ ()=> addToSelectedList(nft)} className="singleNft">
+                { nfts.length ? nfts.map( (nft, i) => <div className="col-lg-4 col-md-4 col-sm-6 col-6">
+                <div onClick={ ()=> addToSelectedList(nft)} className="singleNft">
                     <div className="nftImageBox">
                             <span className="selectNft"><img src={CheckGreen} /></span>
                             <span className="nftImage"><img src={nft.image} /></span>
@@ -51,7 +57,7 @@ function NFTgridView() {
                             <span className="nftNumber">{nft.native.tokenId}</span>
                         </div>
                     </div>
-                </div>) : '' }
+                </div>) : <NFTempty /> }
                 {/* <div className="col-lg-4 col-md-4 col-sm-6 col-6">
                     <div className="singleNft  nftSelect">
                         <div className="nftImageBox">
