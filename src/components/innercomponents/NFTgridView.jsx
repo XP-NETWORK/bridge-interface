508881c6cea87ee8dc74970bc0e025e14ba2bd9f
yurdaD <yurad@xp.network>
2021-11-22 18:37:43 +0200

    nft search in list

--- src/components/innercomponents/NFTgridView.jsx
+++ src/components/innercomponents/NFTgridView.jsx
@@ -6,13 +6,16 @@ import NFT from './NFT';
 function NFTgridView() {
     const nfts = useSelector(state => state.general.NFTList)
     const selectedNFTs = useSelector(state => state.general.selectedNFTList)
+    const search = useSelector(state => state.general.NFTListSearch)
 
     useEffect(() => { }, [selectedNFTs])
 
     return (
         <div className="nftListBox">
             <div className="row">
-                { nfts ? nfts.map((nft, index) => <NFT nft={nft} index={index} />) : <NFTempty /> }
+                {/* chains.filter(chain => chain.text.includes(chainSearch ? chainSearch : '')).map */}
+                { nfts ?  nfts.filter(nft => nft.name.includes(search ? search : '') || nft.native.owner.includes(search ? search : '')).map((nft, index) => <NFT nft={nft} index={index} />) : <NFTempty />}
+                {/* { nfts ? nfts.map((nft, index) => <NFT nft={nft} index={index} />) : <NFTempty /> } */}
             </div>
         </div>
     )
