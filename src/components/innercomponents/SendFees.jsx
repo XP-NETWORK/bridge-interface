cb0462b31f40df7b7c81a9c645dfd96c801f6a7f
yurdaD <yurad@xp.network>
2021-11-27 15:03:24 +0200

    sending from xDai to Ava should get fees in xDai

--- src/components/innercomponents/SendFees.jsx
+++ src/components/innercomponents/SendFees.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 
 function SendFees({ fees }) {
+
+    const from = useSelector(state => state.general.from)
+
     return (
         <div className="nftFees">
-            Fees <span>{fees && fees > 0 ? fees.toFixed(5) : '0'} BNB</span>
+            Fees <span>{fees && fees > 0 ? fees.toFixed(5) : '0'} {from?.text}</span>
         </div>
     )
 }
