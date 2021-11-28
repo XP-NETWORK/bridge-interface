4e0b1f659e579cc1ee1b96070909fd8df24aec29
yurdaD <yurad@xp.network>
2021-11-28 10:05:38 +0200

    nft sceleton

--- src/components/innercomponents/NFT.jsx
+++ src/components/innercomponents/NFT.jsx
@@ -12,6 +12,7 @@ export default function NFT({nft, index}) {
     const dispatch = useDispatch()
     const isSelected = selectedNFTs.filter(n => n.native.tokenId === nft.native.tokenId && n.native.contract === nft.native.contract && n.native.chainId === nft.native.chainId)[0]
     const [imageLoaded, setImageLoaded]= useState(false);
+    const HIDDEN = { visibility: "hidden" };
 
     function addRemoveNFT (chosen){
         if(!isSelected){
@@ -28,17 +29,21 @@ export default function NFT({nft, index}) {
 
     return ( 
         <div className="col-lg-4 col-md-4 col-sm-6 col-6">
-            <div className="singleNft">
+            <div style={ !imageLoaded ? HIDDEN : {}} className="singleNft">
                 <div onClick={() => addRemoveNFT(nft)}  className={`nftImageBox ${isSelected ? 'nftSelect': ''}`}>
                     <span onClick={() => addRemoveNFT(nft)}  className="selectNft">{<img src={CheckGreen} />}</span>
                     <span onClick={() => addRemoveNFT(nft)}  className="nftImage"><img onLoad={() => setImageLoaded(true)} alt="NFT" src={nft.image} /></span>
-                    {!imageLoaded && (
-                          <div className="nft__image--loader"></div>
-                    )}
                 </div>
                     <div className="nftCont">
                     <span className="nftName">{nft.name} <NFTdetails nftInf={nft} index={index} /></span>
                     <span className="nftNumber">{nft.native.tokenId}</span>
+                </div>
+            </div>
+            <div style={ imageLoaded ? HIDDEN : {} } className="nft-sceleton singleNft">
+                <div className="nft-sceleton nftImageBox"></div>
+                <div className="nft-sceleton nftCont">
+                    <span className="nft-sceleton nftName"></span>
+                    <span className="nft-sceleton nftNumber"></span>
                 </div>
             </div>
         </div>
