4560a97e61a8435e1dbf2f94a73e9bed8acd8e6b
Yura Dukhno <yura@Yuras-MBP.fritz.box>
2021-12-23 14:46:51 +0200

    widget-fixes

--- src/components/Widget/index.jsx
+++ src/components/Widget/index.jsx
@@ -1,92 +1,48 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import "./Widget.css"
+import "./WidgetNight.css"
+import "./WidgetLight.css"
 export default function Widget() {
     const reg = new RegExp(/^((0x){0,1}|#{0,1})([0-9A-F]{8}|[0-9A-F]{6})$/ig)
     const from = useSelector(state => state.general.from)
     const to = useSelector(state => state.general.to)
+    const step = useSelector(state => state.general.step)
 
 
-    const changeFontSize = (element, fontSize) => {
-        switch (fontSize) {
-            case 'sm':
-                element.style.fontSize = '14px'
-                break;
-            case 'l':
-                element.style.fontSize = '18px'
-                break;
-            case 'xl':
-                element.style.fontSize = '22px'
-                break;
-            default:
-                break;
-        }
+    const changePalette = palette => {
+        document.body.classList.add(`${palette}-palette`)
     }
-
-    const changeBG = (element, bgColor) => {
-
-        if(reg.test(bgColor)){
-            element.style.backgroundColor = `#${bgColor}`
-        }
-        else{
-            return
-        }
+    
+    const changeFontSize = (fontSize) => {
+        document.body.classList.add(`font-size-${fontSize}`)
     }
-
-    const changeFontColor = (element, color) => {
-        const all = element.getElementsByTagName("*")
-        switch (color) {
-            case "white":
-                Array.prototype.forEach.call(all, (el) => {
-                    el.classList.add("widgetWhite")
-                })
-                break;
-            case "black":
-            Array.prototype.forEach.call(all, (el) => {
-                el.classList.add("widgetBlack")
-            })
-                break;
-            default:
-                break;
-        }
-    }
-
-    const changeButtonColor = color => {
-        document.body.classList.add(`body-btns-${color}`)
-    }
-
     
     useEffect(() => {
         // debugger
         const p = new URLSearchParams(window.location.search)
         const widget = p.get('widget') === 'true'
-        const body = document.querySelector(".bridgeBody")
+        const body = document.getElementsByTagName("body")
         const nftContainer = document.querySelector(".nftContainer")
-        const nftSelectBox = document.querySelector(".nftSelectBox")
-        const themeButton = document.body.querySelector(".themBtn")
-
-        nftSelectBox.style.background = "unset"
+        // const nftSelectBox = document.querySelector(".nftSelectBox")
         
         if(widget) {
-            const backgroundColor = p.get('background')
-            const fontColor = p.get('color')
             const fontSize = p.get('fontsize')
-            const btnColor = p.get("button")
-            fontSize && changeFontSize(nftContainer, fontSize) 
-            backgroundColor && changeBG(body, backgroundColor)
-            fontColor && changeFontColor(nftSelectBox, fontColor)
-            btnColor && changeButtonColor(btnColor)
+            const palette = p.get('palette')
+
+            palette && changePalette(palette);
+            fontSize && changeFontSize(fontSize);
             onlyBridge()
         }
-    },[from, to])
+    },[])
 
     const onlyBridge = () => {
-        document.getElementById('collecSlideCont')?.remove()
-        document.getElementById('footer')?.remove()
-        document.getElementById('aboutnft')?.remove()
-        document.getElementById('tttt')?.remove()
-        document.getElementById('Header')?.remove()
-        document.getElementById('alertb')?.remove()
+        // document.getElementById('collecSlideCont')?.remove()//!Collection in the bottom
+        // document.getElementById('footer')?.remove()
+        // document.getElementById('aboutnft')?.remove()
+        // document.getElementById('tttt')?.remove()//! Tittle
+        // document.getElementById('Header')?.remove() //! Navbar
+        // document.getElementById('alertb')?.remove() //!Alert
         document.body.classList.add('widget')
     }
     return <>
