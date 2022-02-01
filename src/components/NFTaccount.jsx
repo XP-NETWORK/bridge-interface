8a74555ee6c82db518eddc432f484daedc32f2a9
Yura Dukhno <yurad@Yuras-MacBook-Pro.local>
2022-02-01 12:17:08 +0200

    last iotex build

--- src/components/NFTaccount.jsx
+++ src/components/NFTaccount.jsx
@@ -95,7 +95,7 @@ function NFTaccount() {
             // const w = algorandAccount ? algorandAccount : tronWallet ? tronWallet : elrondAccount ? elrondAccount :  
             
             const w = tezosAccount || algorandAccount || tronWallet || elrondAccount || account
-            await setNFTS(hard, from)
+            await setNFTS(w, from)
             } catch (error) {  
                 dispatch(setError(error.data ? error.data.message : error.message))
             }
