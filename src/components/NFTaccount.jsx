7bb49b78608147766d540cfc4831da9aa48dc467
Yura Dukhno <yurad@Yuras-MacBook-Pro.local>
2022-02-02 14:35:36 +0200

    supported <video>  formats

--- src/components/NFTaccount.jsx
+++ src/components/NFTaccount.jsx
@@ -94,7 +94,7 @@ function NFTaccount() {
         try {
             // const w = algorandAccount ? algorandAccount : tronWallet ? tronWallet : elrondAccount ? elrondAccount :  
             const w = tezosAccount || algorandAccount || tronWallet || elrondAccount || account
-            await setNFTS(hard, from)
+            await setNFTS(w, from)
             } catch (error) {  
                 dispatch(setError(error.data ? error.data.message : error.message))
             }
