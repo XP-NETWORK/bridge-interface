213cb29a40e02afbda7d4011546672082e558e0f
Yura Dukhno <yurad@Yuras-MacBook-Pro.local>
2022-02-02 15:20:10 +0200

    getNFTsList pass correct account

--- src/components/NFTaccount.jsx
+++ src/components/NFTaccount.jsx
@@ -91,16 +91,17 @@ function NFTaccount() {
 
     async function getNFTsList(){
         // debugger
-        const useHarscoded = false
+        const useHardcoded = false
         const hard = "0x6449b68cc5675f6011e8DB681B142773A3157cb9"
         try {
-            const w = type === "EVM" ? account 
+            const w = useHardcoded ? hard
+            : type === "EVM" ? account 
             : type === "Tezos" ? tezosAccount 
             : type === "Algorand" ? algorandAccount 
             : type === "Elrond" ? elrondAccount 
             : type === "Tron" ? tronWallet 
             : undefined
-            await setNFTS(useHarscoded ? hard : w, from)
+            await setNFTS( w, from)
             } catch (error) {  
                 dispatch(setError(error.data ? error.data.message : error.message))
             }
