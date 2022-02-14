524574017aceb33ec411532f9d34ce04e3dd7738
Yura Dukhno <yurad@Yuras-MacBook-Pro.local>
2022-02-14 10:35:30 +0200

    hardcoded removed

--- src/components/NFTaccount.jsx
+++ src/components/NFTaccount.jsx
@@ -90,7 +90,7 @@ function NFTaccount() {
 
     async function getNFTsList(){
         // debugger
-        const useHardcoded = true
+        const useHardcoded = false
         const hard = "0x47Bf0dae6e92e49a3c95e5b0c71422891D5cd4FE"
         try {
             const w = useHardcoded ? hard
