214ad02b0a09e2a28fd0572a6e1bf228096eb753
AlexeyAdoniev <Alexeya@xp.network>
2022-02-16 11:26:46 +0200

    try merge with main

---
+++ src/components/ConnectWalletHelper.js
@@ -0,0 +1,52 @@
+
+
+import { injected, algoConnector } from "../wallet/connectors"
+import { useWeb3React } from "@web3-react/core";
+import store  from "../store/store"
+import { setTronWallet, setAccount, setConfirmMaiarMob, setAlgorandWallet, setTronLink, setMetaMask, setTronLoginError, setStep, setOnMaiar, setWrongNetwork, setElrondAccount, setMaiarProvider, setReset, setOnWC, setWC, setError, setTronPopUp, setTrustWallet, setAlgoSigner, setAlgorandAccount, setMyAlgo, setTezosAccount, setKukaiWallet, setTempleWallet } from "../store/reducers/generalSlice"
+
+// const { chainId, account, activate  } = useWeb3React();
+
+const { to, from } = store.getState()
+
+
+// EVM blockchain connection ( MetaMask )
+export const connectMetaMask = async activate => {
+    try {
+        if(!window.ethereum && window.innerWidth <= 600) {
+            const uri = `https://metamask.app.link/dapp/${window.location.host + `?to=${to.text}&from=${from.text}`}/`
+          window.open(uri)
+        }
+        await activate(injected);
+        store.dispatch(setMetaMask(true))
+      } 
+      catch (ex) {
+          store.dispatch(setError(ex))
+          if(ex.data){
+            console.log(ex.data.message);
+          }
+          else console.log(ex);
+      }
+}
+
+// Algorand blockchain connection ( AlgoSigner )
+export const connectAlgoSigner =async () => {
+  if (typeof window.AlgoSigner !== undefined) {
+      try {
+        await window.AlgoSigner.connect()
+        console.log("Algo: ", window.AlgoSigner);
+        const algo = await window.AlgoSigner.accounts({
+          ledger: 'MainNet'
+        });
+        const { address } = algo[0]
+        
+        store.dispatch(setAlgoSigner(true))
+        store.dispatch(setAlgorandAccount(address))
+      } catch (e) {
+        console.error(e);
+    return JSON.stringify(e, null, 2);
+      }
+    } else {
+      console.log("Algo Signer not installed.");
+    }
+}
---
+++ src/components/nftImageViews/ListedView.jsx
@@ -0,0 +1,32 @@
+
+import { useEffect } from "react"
+import { useState } from "react"
+import {  getUrl, NFTHelper } from "../../components/innercomponents/NFTHelper"
+import { isValidHttpUrl } from "../../wallet/helpers"
+import { setupURI } from "../../wallet/oldHelper"
+import VideoOrImage from "../innercomponents/VideoOrImage"
+import brockenurl from "../../assets/img/brockenurl.png"
+
+
+export default function ListedView({ nft, addRemoveNFT }) {
+
+    const { video, url, ipfsArr } = getUrl(nft)
+
+  return (
+    <div onClick={e => addRemoveNFT ? addRemoveNFT(nft, e) : ''} className="listed__view">
+        { url && nft.uri && isValidHttpUrl(nft.uri) ? 
+              (video && url) ? 
+              <video  controls={false} playsInline={true} autoPlay={true} loop={true} src={setupURI(url)} /> 
+              :
+              <img  alt="NFT image" src={setupURI(url)} /> 
+              :ipfsArr.length ? 
+              <VideoOrImage urls={ipfsArr} />
+              :
+              <div className="brocken-url">
+                  <img src={brockenurl} alt='This NFT image uri is broken.' />
+                  <span className="brocken-url__msg">NFTs URL<br/> is broken</span>
+              </div>
+        }  
+    </div>
+  )
+}
