74f0ed46ab2b011bc0bd1c62694707893145ae1a
AlexeyAdoniev <Alexeya@xp.network>
2022-02-17 20:36:25 +0200

    merge testnet

--- src/components/Wallet/TronWallet.jsx
+++ src/components/Wallet/TronWallet.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { connectTronlink } from "../ConnectWalletHelper"
-import Tron from '../../assets/img/wallet/TronLink.svg';
+import React from "react";
+import { useSelector } from "react-redux";
+import { connectTronlink } from "../ConnectWalletHelper";
+import Tron from "../../assets/img/wallet/TronLink.svg";
 
 export default function TronWallet() {
-    const from = useSelector(state => state.general.from)
-    const OFF = { opacity: 0.6, pointerEvents: "none" };
+  const from = useSelector((state) => state.general.from);
+  const OFF = { opacity: 0.6, pointerEvents: "none" };
 
-  return <li onClick={connectTronlink} style={ from ? from.type === "Tron" ? {} : OFF : ""} className="wllListItem"><img src={Tron} alt="Tron Icon" /> TronLink</li>
+  return (
+    <li
+      onClick={connectTronlink}
+      style={from ? (from.type === "Tron" ? {} : OFF) : ""}
+      data-wallet="TronLink"
+      className="wllListItem"
+    >
+      <img src={Tron} alt="Tron Icon" /> TronLink
+    </li>
+  );
 }
