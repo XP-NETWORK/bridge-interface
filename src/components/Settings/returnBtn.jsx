1bb31cd86f51b1e6a883bab927ee5addf9541e0a
AlexeyAdoniev <Alexeya@xp.network>
2022-02-20 23:29:36 +0200

    fix btn

--- src/components/Settings/returnBtn.jsx
+++ src/components/Settings/returnBtn.jsx
@@ -7,11 +7,9 @@ export const ReturnBtn = () => {
   const dispatch = useDispatch();
   const widget = useSelector((state) => state.general.widget);
 
-  return (
-    widget && (
-      <div className="returnBtn">
-        <button onClick={() => dispatch(setReset())}>&#129060;{" Back"}</button>
-      </div>
-    )
-  );
+  return widget ? (
+    <div className="returnBtn">
+      <button onClick={() => dispatch(setReset())}>&#129060;{" Back"}</button>
+    </div>
+  ) : null;
 };
