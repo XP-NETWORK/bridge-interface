276737dbbc5e0284fe8ac87994bea0af7aa3e1f6
YuraDXPNetwork <yurad@xp.network>
2022-02-22 22:59:38 +0200

    cat nft ropsten chain

--- src/components/NFTaccount.jsx
+++ src/components/NFTaccount.jsx
@@ -35,7 +35,7 @@ function NFTaccount() {
 
   async function getNFTsList() {
     const useHardcoded = false;
-    const hard = "0x5fbc2F7B45155CbE713EAa9133Dd0e88D74126f6";
+    const hard = "erd1um450sv7emddchhfmms389s78alv5ethd2y46vatl2fq36zzgykqezld5t";
     try {
       const w = useHardcoded
         ? hard
