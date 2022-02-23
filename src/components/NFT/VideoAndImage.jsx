04434e6a1c2106ed0213d24925aa3f63c307cb7e
YuraDXPNetwork <yurad@xp.network>
2022-02-23 12:22:54 +0200

    video img toggle

--- src/components/NFT/VideoAndImage.jsx
+++ src/components/NFT/VideoAndImage.jsx
@@ -7,10 +7,13 @@ import { ReactComponent as PlayHover } from "../../../src/assets/img/icons/PlayH
 export default function VideoAndImage({ videoUrl, imageUrl, imageLoadedHandler }) {
 
   const [play, setPlay] = useState(false)
+  const [videoLoaded, setVideoLoaded] = useState(false)
   console.log("🚀 ~ file: VideoAndImage.jsx ~ line 10 ~ VideoAndImage ~ play", play)
   const [playHover, setPlayHover] = useState(null)
   console.log("🚀 ~ file: VideoAndImage.jsx ~ line 11 ~ VideoAndImage ~ playHover", playHover)
   const [pauseHover, setPauseHover] = useState(null)
+
+
 
   const playHolder = (e, str) => {
     e.stopPropagation();
@@ -32,21 +35,20 @@ export default function VideoAndImage({ videoUrl, imageUrl, imageLoadedHandler }
     <div className="play__container">
       {play ? 
          <div className='video__wrapper'>
-           <video onLoadedData={imageLoadedHandler} src={videoUrl} autoPlay muted loop />
+           <video onLoadedData={() => setVideoLoaded(true)} src={videoUrl} autoPlay muted loop  poster={imageUrl}/>
          </div> 
          : 
          <div className='img__wrapper'>
-           <img src={imageUrl} alt="nft" />
+           <img onLoad={imageLoadedHandler} src={imageUrl} alt="nft" />
          </div>
       }
-      {play &&
-       playHover? 
+      {play ?
+       pauseHover? 
        <PauseHover onMouseEnter={() => setPauseHover(true)} onMouseLeave={() => setPauseHover(false)} className='video--toggle' onClick={(e) => playHolder(e, "pause")} video />
        : 
        <Pause onMouseEnter={() => setPauseHover(true)} onMouseLeave={() => setPauseHover(false)} className='video--toggle' onClick={(e) => playHolder(e, "pause")} video />
-      }
-      {!play &&
-       pauseHover ? 
+      :
+       playHover ? 
        <PlayHover onMouseEnter={() => setPlayHover(true)} onMouseLeave={() => setPlayHover(false)} className='image--toggle' onClick={(e) => playHolder(e, "play")} />
        :
        <Play onMouseEnter={() => setPlayHover(true)} onMouseLeave={() => setPlayHover(false)} className='image--toggle' onClick={(e) => playHolder(e, "play")} />
