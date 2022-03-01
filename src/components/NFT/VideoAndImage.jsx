4de57750f2e3934c9fc893fbb01ac8e826041f43
YuraDXPNetwork <yurad@xp.network>
2022-03-01 10:32:36 +0200

    play pause fixed and box shadow added

--- src/components/NFT/VideoAndImage.jsx
+++ src/components/NFT/VideoAndImage.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
-import { ReactComponent as Play } from '../../../src/assets/img/icons/Play.svg'
-import { ReactComponent as PlayHover } from "../../../src/assets/img/icons/PlayHover.svg"
-import { ReactComponent as Pause } from '../../../src/assets/img/icons/Pause.svg'
-import { ReactComponent as PauseHover } from '../../../src/assets/img/icons/PauseHover.svg'
-// import { BsFillPlayCircleFill } from "react-icons"
-// import PlayHover from "../../../src/assets/img/icons/play_hover.png"
-// import Play from "../../../src/assets/img/icons/play_default.png"
+import { ReactComponent as Play } from '../../../src/assets/img/icons/_play.svg'
+import { ReactComponent as PlayHover } from "../../../src/assets/img/icons/hover_play.svg"
+import { ReactComponent as Pause } from '../../../src/assets/img/icons/_pause.svg'
+import { ReactComponent as PauseHover } from '../../../src/assets/img/icons/hover_pause.svg'
 import { setupURI } from '../../wallet/oldHelper'
 export default function VideoAndImage({ videoUrl, imageUrl, imageLoadedHandler }) {
 
@@ -46,10 +43,8 @@ export default function VideoAndImage({ videoUrl, imageUrl, imageLoadedHandler }
        <Pause onMouseEnter={() => setPauseHover(true)} onMouseLeave={() => setPauseHover(false)} className='video--toggle' onClick={(e) => playHolder(e, "pause")} video />
       :
        playHover ? 
-      //  <img onMouseEnter={() => setPlayHover(true)} onMouseLeave={() => setPlayHover(false)} style={{width: "25px"}} src={PlayHover} alt="#" className='image--toggle' onClick={(e) => playHolder(e, "play")} /> 
        <PlayHover onMouseEnter={() => setPlayHover(true)} onMouseLeave={() => setPlayHover(false)} className='image--toggle' onClick={(e) => playHolder(e, "play")} />
        :
-      //  <img style={{width: "25px"}} src={Play} alt="#" className='image--toggle' onMouseEnter={() => setPlayHover(true)} onMouseLeave={() => setPlayHover(false)} onClick={(e) => playHolder(e, "play")} />
        <Play onMouseEnter={() => setPlayHover(true)} onMouseLeave={() => setPlayHover(false)} className='image--toggle' onClick={(e) => playHolder(e, "play")} />
       }
     </div>
--- src/components/values.js
+++ src/components/values.js
@@ -218,7 +218,7 @@ export const chains = [
     image: { avatar: true, src: One },
     maintenance: false,
     testNet: true,
-    mainnet: false,
+    mainnet: true,
     newChain: true,
     // off: true
   },
