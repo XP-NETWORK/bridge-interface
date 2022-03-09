94c97174a80efe1570768b189e5ba4a71aa3e5f3
YuraDXPNetwork <yurad@xp.network>
2022-03-09 18:05:43 +0200

    mobile

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -8,9 +8,15 @@ import { setAccountModal, setReset, setWalletsModal } from '../store/reducers/ge
 import "./NavBar.css"
 import { useWeb3React } from '@web3-react/core';
 import Identicon from '../components/Identicon';
+import {LinkContainer} from 'react-router-bootstrap'
+import { useLocation } from 'react-router-dom';
+
+
 
 function NavBar() {
     const dispatch = useDispatch()
+    const location = useLocation().pathname
+    console.log("🚀 ~ file: NavBar.jsx ~ line 19 ~ NavBar ~ location", location)
     const widget = useSelector(state => state.general.widget)
     const handleShow = () => dispatch(setAccountModal(true));
     const step = useSelector(state => state.general.step)
@@ -20,7 +26,7 @@ function NavBar() {
     const algorandAccount = useSelector(state => state.general.algorandAccount)
     const tronWallet = useSelector(state => state.general.tronWallet)
     const { chainId, account } = useWeb3React();
-
+    const style = { background: " rgba(255, 255, 255, 0.4)" }
     const walletAccount = account || elrondAccount || tezosAccount || algorandAccount || tronWallet
 
     const handleConnect = () => {
@@ -38,9 +44,11 @@ function NavBar() {
                 <Navbar.Toggle aria-controls="" />
                     <Navbar.Collapse id="">
                         <Nav>
-                            <Nav.Link  target="_blank" href="https://xp.network/">Home</Nav.Link>
-                            <Nav.Link  target="_blank" href="https://docs.xp.network/">Docs</Nav.Link>
-                            <Nav.Link  target="_blank" href="https://stake.xp.network">Staking</Nav.Link>
+                            <LinkContainer to='/connect'>
+                                <Nav.Link  target="_blank" href="#">Bridge</Nav.Link>
+                            </LinkContainer>
+                            <Nav.Link  target="_blank" href="#">Explorer</Nav.Link>
+                            <Nav.Link  target="_blank" href="#">FAQ</Nav.Link>
                             <div onClick={handleConnect} className='navbar-connect'>
                                 {walletAccount ? `${walletAccount.substring(0, window.innerWidth <= 600 ? 16 : 10)}...${walletAccount.substring(walletAccount.length - 2)}` : "Connect Wallet"}
                                 {walletAccount && <Identicon account={walletAccount} />}
@@ -54,3 +62,8 @@ function NavBar() {
 }
 
 export default NavBar
+
+
+//<Nav.Link  target="_blank" href="https://docs.xp.network/">Docs</Nav.Link>
+//<Nav.Link  target="_blank" href="https://xp.network/">Home</Nav.Link>
+//<Nav.Link  target="_blank" href="https://stake.xp.network">Staking</Nav.Link>
