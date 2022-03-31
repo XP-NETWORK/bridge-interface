0f9e1e174cae5d484f4f62a1d7ab0130bfd980a8
YuraDXPNetwork <yurad@xp.network>
2022-03-31 16:37:51 +0300

    wallet order

--- src/components/Wallet/WalletList.jsx
+++ src/components/Wallet/WalletList.jsx
@@ -15,16 +15,16 @@ export default function WalletList({search, connected, input}) {
 
     const walletComponents = [
         { Component: <EVMWallet wallet={"MetaMask"} key="wallet-index-0" close={connected} />, name: "MetaMask", type: "EVM", mobile: true, desktop: true },
-        { Component: <EVMWallet wallet={undefined} key="wallet-index-1" close={connected} />, name: "WalletConnect", type: "EVM", mobile: false, desktop: false },
         { Component: <EVMWallet wallet={"TrustWallet"} key="wallet-index-2" close={connected} />, name: "Trust Wallet", type: "EVM", mobile:true, desktop: false },
+        { Component: <EVMWallet wallet={undefined} key="wallet-index-1" close={connected} />, name: "WalletConnect", type: "EVM", mobile: false, desktop: false },
+        { Component: <TezosWallet wallet={"TempleWallet"} key="wallet-index-7" close={connected} />, name: "Temple Wallet Tezos", type: "Tezos", mobile: true, desktop: true },
+        { Component: <TezosWallet wallet={undefined} key="wallet-index-8" close={connected} />, name: "Beacon Tezos", type: "Tezos", mobile: true, desktop: true },
+        { Component: <ElrondWallet wallet={"Maiar"} key="wallet-index-9" close={connected} />, name: "Maiar Elrond", type: "Elrond", mobile: true, desktop: true },
+        { Component: <ElrondWallet wallet={undefined} key="wallet-index-10" close={connected} />, name: "Maiar Extension Elrond", type: "Elrond", mobile: false, desktop: true },
         { Component: <AlgorandWallet wallet={"MyAlgo"} key="wallet-index-3" close={connected} />, name: "MyAlgo", type: "Algorand", mobile:false, desktop: true },
         { Component: <AlgorandWallet wallet={"AlgoSigner"} key="wallet-index-4" close={connected} />, name: "AlgoSigner", type: "Algorand", mobile:false, desktop: true },
         { Component: <AlgorandWallet wallet={undefined} key="wallet-index-5" close={connected} />, name: "Algorand Wallet", type: "Algorand", mobile:false, desktop: false },
         { Component: <TronWallet key="wallet-index-6" close={connected} />, name: "TronLink", type: "Tron", mobile:false, desktop: true},
-        { Component: <TezosWallet wallet={"TempleWallet"} key="wallet-index-7" close={connected} />, name: "Temple Wallet Tezos", type: "Tezos", mobile: true, desktop: true },
-        { Component: <TezosWallet wallet={undefined} key="wallet-index-8" close={connected} />, name: "Beacon Tezos", type: "Tezos", mobile: true, desktop: true },
-        { Component: <ElrondWallet wallet={"Maiar"} key="wallet-index-9" close={connected} />, name: "Maiar Elrond", type: "Elrond", mobile: true, desktop: false },
-        { Component: <ElrondWallet wallet={undefined} key="wallet-index-10" close={connected} />, name: "Maiar Extension Elrond", type: "Elrond", mobile: false, desktop: true },
         { Component: <USBWallet wallet={"Ledger"} key="wallet-index-11" connected={connected} />, name: "Ledger", type: '' },
         { Component: <USBWallet key="wallet-index12" connected={connected} />, name: "Trezor", type: '' }
      ]
