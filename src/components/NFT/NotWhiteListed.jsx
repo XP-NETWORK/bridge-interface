1d71057df4b6eeb5e7f2e1f7cfe75b16ed2d7d33
YuraDXPNetwork <yurad@xp.network>
2022-04-07 13:16:52 +0300

    not whitelisted covers image container only

--- src/components/NFT/NotWhiteListed.jsx
+++ src/components/NFT/NotWhiteListed.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function NotWhiteListed() {
   return (
-    <div className="nft-box__container not-whitelisted">
+    <div className="image__wrapper not-whitelisted">
         <div className="not-whitelisted__wrapper">
             <span className='not-whitelisted__text'>NFT is not Whitelisted</span>
             <a className='not-whitelisted__button' href='https://t.me/XP_NETWORK_Bridge_Support_Bot?start=startwithxpbot' target="_blank" rel="noreferrer">Tech support</a>
