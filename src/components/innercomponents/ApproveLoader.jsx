c1078d8c7a911af26aa897f1467dee972b4027af
YuraDXPNetwork <yurad@xp.network>
2022-04-11 14:03:58 +0300

    approve modal planet for ios don't fixed

--- src/components/innercomponents/ApproveLoader.jsx
+++ src/components/innercomponents/ApproveLoader.jsx
@@ -16,7 +16,7 @@ export default function ApproveLoader() {
           overflow: "hidden",
         }} 
         animation={true}
-        show={loader}>
+        show={true}>
             <Planet />
         </Modal>
     )
