713a31c59c4044e3c3d1e9b7783bac19c5941fc3
YuraDXPNetwork <yurad@xp.network>
2022-04-11 14:27:19 +0300

    all wallet names in center of container

--- src/components/Wallet/EVMWallet.jsx
+++ src/components/Wallet/EVMWallet.jsx
@@ -5,20 +5,15 @@ import { useDispatch, useSelector } from "react-redux";
 import MetaMask from "../../assets/img/wallet/MetaMask.svg";
 import WalletConnect from "../../assets/img/wallet/WalletConnect 3.svg";
 import TrustWallet from "../../assets/img/wallet/TWT.svg";
-import { isEVM } from "../../wallet/oldHelper";
-import { setAccount, setMetaMask, setWrongNetwork } from "../../store/reducers/generalSlice";
-import { CHAIN_INFO, TESTNET_CHAIN_INFO } from "../values";
-import { setNFTS } from "../../wallet/helpers";
+import { setAccount, setMetaMask } from "../../store/reducers/generalSlice";
 
 export default function EVMWallet({ wallet, close }) {
-  const { chainId, account, activate } = useWeb3React();
+  const {  account, activate } = useWeb3React();
   const OFF = { opacity: 0.6, pointerEvents: "none" };
-  // console.log("🚀 ~ file: EVMWallet.jsx ~ line 15 ~ EVMWallet ~ chainId", chainId)
   const from = useSelector(state => state.general.from);
   const to = useSelector(state => state.general.to);
   const dispatch = useDispatch()
   const getMobOps = () =>  /android/i.test(navigator.userAgent || navigator.vendor || window.opera) ? true : false;
-  const testnet = useSelector((state) => state.general.testNet);
 
   
   const connectHandler = async wallet => {
