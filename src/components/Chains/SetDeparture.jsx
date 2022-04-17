cd2aa89f226bfc2f6221757a7b6837f0d0318046
YuraDXPNetwork <yurad@xp.network>
2022-04-17 23:06:54 +0300

    jazzicin

--- src/components/Chains/SetDeparture.jsx
+++ src/components/Chains/SetDeparture.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { setChainModal, setDepartureOrDestination } from '../../store/reducers/generalSlice';
 import Departure from '../../assets/img/nftSelect/departure.svg';
+import Jazzicon from 'react-jazzicon'
 
 export default function SetDeparture() {
     const dispatch = useDispatch()
     const from = useSelector(state => state.general.from)
+
+    const [seed, setSeed] = useState()
+
+
     
     const handleShow = (str) => {
         dispatch(setChainModal(true));
@@ -21,15 +26,22 @@ export default function SetDeparture() {
         }
     }
 
+    useEffect(() => {
+        const _seed = Math.round(Math.random() * 10000000)
+        if(!seed)setSeed(_seed)
+    }, [])
+
   return (
         <div className="selChain seleDepat" onClick={() => handleShow("departure")}>
             { from ? 
                 <div className="seleDepatSelec">
-                    <img src={ from.image.src } alt="" />{from.text === "xDai" ? "Gnosis" : from.text}
+                    <Jazzicon diameter={30} seed={seed} />
+                    <div className="select-chain__text">{from.text === "xDai" ? "Gnosis" : from.text}</div>
                 </div>
                 :
                 <div className="seleDepatSelec">
-                    <img src={ Departure } alt="" />{window.innerWidth >= 600 ? "Select Departure chain" : "Departure chain"}
+                    <Jazzicon diameter={30} seed={seed} />
+                    <div className="select-chain__text">{window.innerWidth >= 600 ? "Select Departure chain" : "Departure chain"}</div>
                 </div>
             }
         </div>
--- src/components/Chains/SetDestination.jsx
+++ src/components/Chains/SetDestination.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux'
 import Destination from '../../assets/img/nftSelect/destination.svg';
 import { setChainModal, setDepartureOrDestination } from '../../store/reducers/generalSlice';
+import Jazzicon from 'react-jazzicon'
 
 export default function SetDestination() {
     const to = useSelector(state => state.general.to)
     const dispatch = useDispatch()
+
+    const [seed, setSeed] = useState()
+
+    useEffect(() => {
+        const _seed = Math.round(Math.random() * 10000000)
+        if(!seed)setSeed(_seed)
+    }, [])
+    
+
+
     
     const handleShow = (str) => {
         dispatch(setChainModal(true));
@@ -16,11 +27,13 @@ export default function SetDestination() {
     <div className="selChain seleDesti" onClick={() => handleShow("destination")}>
     { to ?
         <div className="seleDestiSele">
-            <img  style={{width: "28px"}} src={ to.image.src } alt="" />{to.text === "xDai" ? "Gnosis" : to.text}
+            <Jazzicon diameter={30} seed={seed} />
+            {to.text === "xDai" ? "Gnosis" : to.text}
         </div>
         :
         <div className="seleDestiSele">
-            <img  style={{width: "28px"}} src={ Destination } alt="" />{window.innerWidth >= 600 ? "Select Destination chain" : "Destination chain"}
+            <Jazzicon diameter={30} seed={seed} />
+            {window.innerWidth >= 600 ? "Select Destination chain" : "Destination chain"}
         </div>
     }
 </div>
