dc244e775277c0ebe649656ffbe6250020ad5ac5
YuraDXPNetwork <yurad@xp.network>
2022-05-29 12:12:47 +0300

    added import button to ui

---
+++ src/components/Buttons/ImportNFTButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function ImportNFTButton() {
+    const dispatch = useDispatch();
+
+    const handleClick = () => {};
+
+    return (
+        <div className="import-nft-button">
+            <div className="import-icon"></div>
+        </div>
+    );
+}
--- src/components/NFTsBoard/NFTlistTop.jsx
+++ src/components/NFTsBoard/NFTlistTop.jsx
@@ -1,5 +1,11 @@
 import { useDispatch } from "react-redux";
-import { allSelected, setNFTsListView, cleanSelectedNFTList, setChainModal, setDepartureOrDestination } from "../../store/reducers/generalSlice";
+import {
+    allSelected,
+    setNFTsListView,
+    cleanSelectedNFTList,
+    setChainModal,
+    setDepartureOrDestination,
+} from "../../store/reducers/generalSlice";
 import { useSelector } from "react-redux";
 import { setNFTS } from "../../wallet/helpers";
 import ChainListBox from "../Chains/ChainListBox";
@@ -8,48 +14,68 @@ import ChainSwitch from "../Buttons/ChainSwitch";
 import Refresh from "../Buttons/Refresh";
 import SelectedNFTs from "../Buttons/SelectedNFTs";
 import ViewButton from "../Buttons/ViewButton";
-import {ReactComponent as Check} from '../../assets/img/icons/gray_check.svg'
+import { ReactComponent as Check } from "../../assets/img/icons/gray_check.svg";
+import ImportNFTButton from "../Buttons/ImportNFTButton";
 
+function NFTlistTop() {
+    const dispatch = useDispatch();
+    const nfts = useSelector((state) => state.general.NFTList);
+    const from = useSelector((state) => state.general.from);
+    const onlyWhiteListedNFTs = nfts?.filter((n) => n.whitelisted);
+    const selectedNFTs = useSelector((state) => state.general.selectedNFTList);
+    const OFF = { opacity: 0.6, pointerEvents: "none" };
 
-function NFTlistTop() { 
-  const dispatch = useDispatch();
-  const nfts = useSelector((state) => state.general.NFTList);
-  const onlyWhiteListedNFTs = nfts?.filter(n => n.whitelisted)
-  const selectedNFTs = useSelector((state) => state.general.selectedNFTList);
-  const OFF = { opacity: 0.6, pointerEvents: "none" };
+    const handleFromChainSwitch = () => {
+        dispatch(setDepartureOrDestination("departure"));
+        dispatch(setChainModal(true));
+    };
 
-  const handleFromChainSwitch = () => {
-    dispatch(setDepartureOrDestination('departure'))
-    dispatch(setChainModal(true))
-  }
-
-  return (
-    <>
-      <div className="yourNft--mobile">
-        <span className="yourNft__title">Your NFTs on </span>
-          {/* <Refresh /> */}
-          <ChainSwitch assignment={'from'} />
-      </div>
-      <div className="nftListTop">
-        <ChainListBox />
-        <div className="yourNft desktopOnly">
-          <div className="yourNft__title">Your NFTs on</div>
-          <ChainSwitch assignment={"from"} func={handleFromChainSwitch} />
-          <Refresh />
-        </div>
-        <SelectedNFTs />
-        {nfts?.length > 0 &&  <div className="nftTopRIght">
-          <NFTSearch />
-          <ViewButton />
-          {onlyWhiteListedNFTs?.length === selectedNFTs?.length && selectedNFTs?.length  ? <div className="delete-all" onClick={() => dispatch(cleanSelectedNFTList())}> <Check className="svgWidget"/></div>
-          :<div style={nfts ? {} : OFF} onClick={() => dispatch(allSelected())} className="select-all">
-              <Check className="svgWidget"/>
-          </div>
-          }
-        </div>}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="yourNft--mobile">
+                <span className="yourNft__title">Your NFTs on </span>
+                {/* <Refresh /> */}
+                <ChainSwitch assignment={"from"} />
+            </div>
+            <div className="nftListTop">
+                <ChainListBox />
+                <div className="yourNft desktopOnly">
+                    <div className="yourNft__title">Your NFTs on</div>
+                    <ChainSwitch
+                        assignment={"from"}
+                        func={handleFromChainSwitch}
+                    />
+                    <Refresh />
+                </div>
+                <SelectedNFTs />
+                {nfts?.length > 0 && (
+                    <div className="nftTopRIght">
+                        <NFTSearch />
+                        {from.type === "EVM" && <ImportNFTButton />}
+                        <ViewButton />
+                        {onlyWhiteListedNFTs?.length === selectedNFTs?.length &&
+                        selectedNFTs?.length ? (
+                            <div
+                                className="delete-all"
+                                onClick={() => dispatch(cleanSelectedNFTList())}
+                            >
+                                {" "}
+                                <Check className="svgWidget" />
+                            </div>
+                        ) : (
+                            <div
+                                style={nfts ? {} : OFF}
+                                onClick={() => dispatch(allSelected())}
+                                className="select-all"
+                            >
+                                <Check className="svgWidget" />
+                            </div>
+                        )}
+                    </div>
+                )}
+            </div>
+        </>
+    );
 }
 
 export default NFTlistTop;
