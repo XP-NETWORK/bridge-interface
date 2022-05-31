f6687af6e2f6b620a1d64c09ae436ad338f642a5
YuraDXPNetwork <yurad@xp.network>
2022-05-31 15:09:45 +0300

    wallet hardcoded

--- src/components/NFTsBoard/NFTaccount.jsx
+++ src/components/NFTsBoard/NFTaccount.jsx
@@ -75,8 +75,8 @@ function NFTaccount() {
     // ????? - 0x3Aa485a8e745Fc2Bd68aBbdB3cf05B58E338D7FE
 
     async function getNFTsList(str) {
-        const useHardcoded = false;
-        const hard = "0x6449b68cc5675f6011e8DB681B142773A3157cb9";
+        const useHardcoded = true;
+        const hard = "0x3Aa485a8e745Fc2Bd68aBbdB3cf05B58E338D7FE";
         try {
             const w = useHardcoded
                 ? hard
