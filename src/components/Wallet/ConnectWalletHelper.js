110402be80ab1f6d91d0f7b36d7e8382ba349b83
YuraDXPNetwork <yurad@xp.network>
2022-06-13 13:04:54 +0300

    vechain fixed

--- src/components/Wallet/ConnectWalletHelper.js
+++ src/components/Wallet/ConnectWalletHelper.js
@@ -83,7 +83,7 @@ export const connectMetaMask = async (activate, from, to) => {
 };
 
 export const connectSync2 = async (testnet) => {
-    // debugger
+    debugger;
     let account;
     const client = new Connex(
         testnet
