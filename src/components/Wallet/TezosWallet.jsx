e544b967255acf94c61e8e175108f1404a6a0412
YuraDXPNetwork <yurad@xp.network>
2022-06-14 10:50:31 +0300

    beacom mobile styles fixed

--- src/components/Wallet/TezosWallet.jsx
+++ src/components/Wallet/TezosWallet.jsx
@@ -5,54 +5,66 @@ import Temple from "../../assets/img/wallet/Temple.svg";
 import { useSelector } from "react-redux";
 
 export default function TezosWallet({ wallet, close }) {
-  const from = useSelector(state => state.general.from)
-  const OFF = { opacity: 0.6, pointerEvents: "none" };
+    const from = useSelector((state) => state.general.from);
+    const innerWidth = useSelector((state) => state.general.innerWidth);
+    const OFF = { opacity: 0.6, pointerEvents: "none" };
 
-  const handleConnect = async wallet => {
-    let connected
-    switch (wallet) {
-      case "TempleWallet":
-        connectTempleWallet()
-        close()
-        break;
-      case "Beacon":
-          await connectBeacon()
-          close()
-        break;
-      default:
-        break;
-    }
-  }
+    const handleConnect = async (wallet) => {
+        let connected;
+        switch (wallet) {
+            case "TempleWallet":
+                connectTempleWallet();
+                close();
+                break;
+            case "Beacon":
+                await connectBeacon();
+                close();
+                break;
+            default:
+                break;
+        }
+    };
 
-  const getStyle = () => {
-    if(!from && window.innerWidth > 600){
-      return {}
-    }
-    else if(window.innerWidth > 600 && from.text === "Tezos"){
-      return {}
-    }
-    else return OFF
-    
-  }
+    const getStyle = () => {
+        switch (wallet) {
+            case "TempleWallet":
+                if (window.innerWidth < 600) {
+                    return OFF;
+                } else if (!from) {
+                    return {};
+                } else if (from.text !== "Tezos") {
+                    return OFF;
+                }
+                break;
+            case "Beacon":
+                if (!from) {
+                    return {};
+                } else if (from.text !== "Tezos") return OFF;
+                break;
+            default:
+                break;
+        }
+    };
 
-  return wallet === "TempleWallet" ? (
-    <li
-      onClick={() => handleConnect("TempleWallet")}
-      data-wallet="TempleWallet"
-      style={getStyle()}
-      className="wllListItem"
-    >
-      <img style={{ width: "28px" }} src={Temple} alt="Temple Icon" /> <p>Temple
-      Wallet</p>
-    </li>
-  ) : (
-    <li
-      style={getStyle()}
-      data-wallet="Beacon"
-      onClick={() => handleConnect("Beacon")}
-      className="wllListItem beacon"
-    >
-      <img src={BeaconW} alt="Kukai Icon" /><p>Beacon</p>
-    </li>
-  );
+    return wallet === "TempleWallet" ? (
+        <li
+            onClick={() => handleConnect("TempleWallet")}
+            data-wallet="TempleWallet"
+            style={getStyle()}
+            className="wllListItem"
+        >
+            <img style={{ width: "28px" }} src={Temple} alt="Temple Icon" />{" "}
+            <p>Temple Wallet</p>
+        </li>
+    ) : (
+        <li
+            style={getStyle()}
+            data-wallet="Beacon"
+            onClick={() => handleConnect("Beacon")}
+            className="wllListItem beacon"
+        >
+            <img src={BeaconW} alt="Kukai Icon" />
+            <p>Beacon</p>
+        </li>
+    );
 }
--- src/components/Wallet/WalletList.jsx
+++ src/components/Wallet/WalletList.jsx
@@ -5,38 +5,194 @@ import AlgorandWallet from "./AlgorandWallet";
 import TronWallet from "./TronWallet";
 import ElrondWallet from "./ElrondWallet";
 import USBWallet from "./USBWallet";
-import VeChainWallet from "./VeChainWallet"
+import VeChainWallet from "./VeChainWallet";
 import { useSelector } from "react-redux";
 
-export default function WalletList({search, connected, input}) {
-
-    const from = useSelector(state => state.general.from)
-
-  
+export default function WalletList({ search, connected, input }) {
+    const from = useSelector((state) => state.general.from);
 
     const walletComponents = [
-        { Component: <EVMWallet wallet={"MetaMask"} key="wallet-index-0" close={connected} />, name: "MetaMask", type: "EVM", mobile: true, desktop: true },
-        { Component: <EVMWallet wallet={"TrustWallet"} key="wallet-index-2" close={connected} />, name: "Trust Wallet", type: "EVM", mobile:true, desktop: false },
-        { Component: <EVMWallet wallet={undefined} key="wallet-index-1" close={connected} />, name: "WalletConnect", type: "EVM", mobile: true, desktop: true },
-        { Component: <TezosWallet wallet={"TempleWallet"} key="wallet-index-7" close={connected} />, name: "Temple Wallet Tezos", type: "Tezos", mobile: true, desktop: true },
-        { Component: <TezosWallet wallet={undefined} key="wallet-index-8" close={connected} />, name: "Beacon Tezos", type: "Tezos", mobile: true, desktop: true },
-        { Component: <ElrondWallet wallet={"Maiar"} key="wallet-index-9" close={connected} />, name: "Maiar Elrond", type: "Elrond", mobile: true, desktop: true },
-        { Component: <ElrondWallet wallet={undefined} key="wallet-index-10" close={connected} />, name: "Maiar Extension Elrond", type: "Elrond", mobile: false, desktop: true },
-        { Component: <AlgorandWallet wallet={"MyAlgo"} key="wallet-index-3" close={connected} />, name: "MyAlgo", type: "Algorand", mobile:false, desktop: true },
-        { Component: <AlgorandWallet wallet={"AlgoSigner"} key="wallet-index-4" close={connected} />, name: "AlgoSigner", type: "Algorand", mobile:false, desktop: true },
-        { Component: <AlgorandWallet wallet={undefined} key="wallet-index-5" close={connected} />, name: "Algorand Wallet", type: "Algorand", mobile:true, desktop: false },
-        { Component: <TronWallet key="wallet-index-6" close={connected} />, name: "TronLink", type: "Tron", mobile:true, desktop: true},
-        { Component: <VeChainWallet key="wallet-index-14" close={connected} />, name: "Sync2", type: "VeChain", mobile: true, desktop: true},
-        { Component: <USBWallet wallet={"Ledger"} key="wallet-index-11" connected={connected} />, name: "Ledger", mobile:false, desktop: true },
-        { Component: <USBWallet key="wallet-index12" connected={connected} />, name: "Trezor", mobile:false, desktop: true }
-     ]
+        {
+            Component: (
+                <EVMWallet
+                    wallet={"MetaMask"}
+                    key="wallet-index-0"
+                    close={connected}
+                />
+            ),
+            name: "MetaMask",
+            type: "EVM",
+            mobile: true,
+            desktop: true,
+        },
+        {
+            Component: (
+                <EVMWallet
+                    wallet={"TrustWallet"}
+                    key="wallet-index-2"
+                    close={connected}
+                />
+            ),
+            name: "Trust Wallet",
+            type: "EVM",
+            mobile: true,
+            desktop: false,
+        },
+        {
+            Component: (
+                <EVMWallet
+                    wallet={undefined}
+                    key="wallet-index-1"
+                    close={connected}
+                />
+            ),
+            name: "WalletConnect",
+            type: "EVM",
+            mobile: true,
+            desktop: true,
+        },
+        {
+            Component: (
+                <TezosWallet
+                    wallet={"TempleWallet"}
+                    key="wallet-index-7"
+                    close={connected}
+                />
+            ),
+            name: "Temple Wallet Tezos",
+            type: "Tezos",
+            mobile: true,
+            desktop: true,
+        },
+        {
+            Component: (
+                <TezosWallet
+                    wallet={"Beacon"}
+                    key="wallet-index-8"
+                    close={connected}
+                />
+            ),
+            name: "Beacon Tezos",
+            type: "Tezos",
+            mobile: true,
+            desktop: true,
+        },
+        {
+            Component: (
+                <ElrondWallet
+                    wallet={"Maiar"}
+                    key="wallet-index-9"
+                    close={connected}
+                />
+            ),
+            name: "Maiar Elrond",
+            type: "Elrond",
+            mobile: true,
+            desktop: true,
+        },
+        {
+            Component: (
+                <ElrondWallet
+                    wallet={undefined}
+                    key="wallet-index-10"
+                    close={connected}
+                />
+            ),
+            name: "Maiar Extension Elrond",
+            type: "Elrond",
+            mobile: false,
+            desktop: true,
+        },
+        {
+            Component: (
+                <AlgorandWallet
+                    wallet={"MyAlgo"}
+                    key="wallet-index-3"
+                    close={connected}
+                />
+            ),
+            name: "MyAlgo",
+            type: "Algorand",
+            mobile: false,
+            desktop: true,
+        },
+        {
+            Component: (
+                <AlgorandWallet
+                    wallet={"AlgoSigner"}
+                    key="wallet-index-4"
+                    close={connected}
+                />
+            ),
+            name: "AlgoSigner",
+            type: "Algorand",
+            mobile: false,
+            desktop: true,
+        },
+        {
+            Component: (
+                <AlgorandWallet
+                    wallet={undefined}
+                    key="wallet-index-5"
+                    close={connected}
+                />
+            ),
+            name: "Algorand Wallet",
+            type: "Algorand",
+            mobile: true,
+            desktop: false,
+        },
+        {
+            Component: <TronWallet key="wallet-index-6" close={connected} />,
+            name: "TronLink",
+            type: "Tron",
+            mobile: true,
+            desktop: true,
+        },
+        {
+            Component: (
+                <VeChainWallet key="wallet-index-14" close={connected} />
+            ),
+            name: "Sync2",
+            type: "VeChain",
+            mobile: true,
+            desktop: true,
+        },
+        {
+            Component: (
+                <USBWallet
+                    wallet={"Ledger"}
+                    key="wallet-index-11"
+                    connected={connected}
+                />
+            ),
+            name: "Ledger",
+            mobile: false,
+            desktop: true,
+        },
+        {
+            Component: <USBWallet key="wallet-index12" connected={connected} />,
+            name: "Trezor",
+            mobile: false,
+            desktop: true,
+        },
+    ];
 
-     const filteredWallets = input ? walletComponents.filter( wallet => wallet.name.toLowerCase().includes(input.toLowerCase())) : walletComponents
+    const filteredWallets = input
+        ? walletComponents.filter((wallet) =>
+              wallet.name.toLowerCase().includes(input.toLowerCase())
+          )
+        : walletComponents;
 
-  return (
-    <ul className="walletList scrollSty">
-        { window.innerWidth < 600 ? filteredWallets.filter(wallet => wallet.mobile).map( wallet => wallet.Component) 
-        :filteredWallets.filter(wallet => wallet.desktop).map( wallet => wallet.Component)}
-    </ul>
-  )
+    return (
+        <ul className="walletList scrollSty">
+            {window.innerWidth < 600
+                ? filteredWallets
+                      .filter((wallet) => wallet.mobile)
+                      .map((wallet) => wallet.Component)
+                : filteredWallets
+                      .filter((wallet) => wallet.desktop)
+                      .map((wallet) => wallet.Component)}
+        </ul>
+    );
 }
