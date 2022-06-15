7a411529035ed2efbe2f104e5cceb6bceb198533
YuraDXPNetwork <yurad@xp.network>
2022-06-15 09:14:47 +0300

    evm compare chain id's in connection

--- src/components/NFTsBoard/NFTaccount.jsx
+++ src/components/NFTsBoard/NFTaccount.jsx
@@ -80,7 +80,7 @@ function NFTaccount() {
     // ????? - 0x3Aa485a8e745Fc2Bd68aBbdB3cf05B58E338D7FE
 
     async function getNFTsList(str) {
-        const useHardcoded = true;
+        const useHardcoded = false;
         const hard = "0x553af319d04732c93d74132ff906e961be268161";
         try {
             const w = useHardcoded
