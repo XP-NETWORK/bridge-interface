44466c73b2a813d67eec983274e187e04dbb269b
YuraDXPNetwork <yurad@xp.network>
2022-07-04 15:30:57 +0200

    pages

--- src/components/NFTsBoard/Pagination.jsx
+++ src/components/NFTsBoard/Pagination.jsx
@@ -5,14 +5,27 @@ export default function Pagination() {
     const dispatch = useDispatch();
     const nfts = useSelector((state) => state.general.NFTList);
 
+    const currentPageClick = (i) => {
+        debugger;
+        console.log(i);
+    };
+
     return (
         <div className="pagination">
             <div className="pagination__prev">&#10094;</div>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>...</div>
-            <div>{(nfts?.length / 6) % 6}</div>
+            <ul className="pagination__list">
+                {nfts?.map((e, i) => {
+                    if ((i / 6) % 6 === 0) {
+                        return (
+                            <li onClick={() => currentPageClick(i)}>
+                                {i > 0 ? nfts.length / i : i}
+                            </li>
+                        );
+                    }
+                })}
+                {(nfts?.length / 6) % 6 > 3 && <div>...</div>}
+                <div>{(nfts?.length / 6) % 6}</div>
+            </ul>
             <div className="pagination__next">&#10095;</div>
         </div>
     );
