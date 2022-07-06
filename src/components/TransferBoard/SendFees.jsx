1c1511a61ac514cdfd9134ddcfca364aad9a8e7b
AlexeyAdoniev <Alexeya@xp.network>
2022-07-06 18:49:33 +0300

    merge

--- src/components/TransferBoard/SendFees.jsx
+++ src/components/TransferBoard/SendFees.jsx
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import BigNumber from 'bignumber.js';
-import React, { useState } from 'react'
-import { LittleLoader } from '../innercomponents/LittleLoader';
-import { useDispatch, useSelector } from 'react-redux';
-import { chainsConfig, CHAIN_INFO } from '../values';
-import { getFactory,  handleChainFactory,  setClaimablesAlgorand, setNFTS } from "../../wallet/helpers"
-import { setBigNumFees } from '../../store/reducers/generalSlice';
-import { setOriginalFee } from '../../store/reducers/settingsSlice';
-import { useEffect } from 'react';
-import { useWeb3React } from "@web3-react/core";
-
-function SendFees() {
-    const dispatch = useDispatch()
-    const balance = useSelector(state => state.general.balance)
-    const to = useSelector(state => state.general.to)
-    const from = useSelector(state => state.general.from)
-    const account = useSelector(state => state.general.account)
-    const widget = useSelector(state => state.general.widget)
-    const affiliationFees = useSelector(state => state.settings.affiliationFees)
-    const selectedNFTList = useSelector(state => state.general.selectedNFTList)
-    const isToEVM = useSelector(state => state.general.to).type === 'EVM'
-    const [fees, setFees ] = useState('')
-    const Web3Utils = require("web3-utils");
-    const [estimateInterval, setEstimateInterval] = useState()
-    const [loading, setLoading] = useState(false)
-=======
 import BigNumber from "bignumber.js";
 import React, { useState } from "react";
 import { LittleLoader } from "../innercomponents/LittleLoader";
@@ -52,7 +25,6 @@ function SendFees() {
   const Web3Utils = require("web3-utils");
   const [estimateInterval, setEstimateInterval] = useState();
   const [loading, setLoading] = useState(false);
->>>>>>> new-ui
 
   async function estimate() {
     let fact;
@@ -73,69 +45,6 @@ function SendFees() {
           ? "0x5fbc2F7B45155CbE713EAa9133Dd0e88D74126f6"
           : account;
 
-<<<<<<< HEAD
-
-    async function estimate () {
-        let fact
-        let fee
-        try {
-            const fromChain = await handleChainFactory(from.text)
-            const toChain = await handleChainFactory(to.text)
-            const wallet = 
-            to ==='Tron' ? 'TCCKoPRcYoCGkxVThCaY9vRPaKiTjE4x1C' 
-            : from === 'Tron' && isToEVM ? '0x5fbc2F7B45155CbE713EAa9133Dd0e88D74126f6'
-            : from === 'Algorand' && isToEVM ? '0x5fbc2F7B45155CbE713EAa9133Dd0e88D74126f6'
-            : from === 'Elrond' && isToEVM ? '0x5fbc2F7B45155CbE713EAa9133Dd0e88D74126f6'
-            : from === 'Tezos' && isToEVM ? '0x5fbc2F7B45155CbE713EAa9133Dd0e88D74126f6' 
-            : account;
-
-            fact = await getFactory()
-            if(selectedNFTList.length) {
-                if(to ==='Tron'){
-                   fee = from === 'BSC' ? new BigNumber('100000000000000000')
-                    : from === 'Polygon' ? new BigNumber('23200000000000000000') 
-                    : from === 'Ethereum' ? new BigNumber('14952490000000000') 
-                    : from === 'Algorand' ? new BigNumber('32160950300000000000') 
-                    : from === 'Elrond' ? new BigNumber('239344350000000000') 
-                    : from === 'Avalanche' ? new BigNumber('529683610000000000') 
-                    : from === 'xDai' ? new BigNumber('56645012600000000000') 
-                    : from === 'Fuse' ? new BigNumber('95352570490000000000') 
-                    : ''
-                }
-                else{
-                    try {
-                       fee = await fact.estimateFees(fromChain, toChain, selectedNFTList[0], wallet)
-                    } catch (error) {
-                        console.error(error);
-                    }
-                } 
-            }
-            let bigNum = fee ? fee.multipliedBy(1.1): undefined;//.integerValue().toString(10) : undefined;
-           
-     
-
-            if (bigNum && widget && affiliationFees && Number(affiliationFees)/100 + 1 > 1) {
-                const feesMultiplier = Number(affiliationFees)/100 + 1;
-                bigNum = bigNum.multipliedBy(feesMultiplier <= 2 ? feesMultiplier : 2);
-            }
-           
-            bigNum = bigNum? bigNum.integerValue().toString(10): undefined;
-  
-
-          
-            dispatch(setBigNumFees(bigNum))
-            let fees
-            if(from.type === "Tron" || from.type === "Algorand"){
-                fees = bigNum / 1e6
-            }
-            else{
-                fees =  await Web3Utils.fromWei(bigNum, "ether")
-            }
-            setFees(+(fees*selectedNFTList.length))
-           
-        } catch (error) {
-          console.log(error.data ? error.data.message : error.message);
-=======
       fact = await getFactory();
       if (selectedNFTList.length) {
         if (to === "Tron") {
@@ -168,7 +77,6 @@ function SendFees() {
           } catch (error) {
             console.error(error);
           }
->>>>>>> new-ui
         }
       }
 
@@ -206,21 +114,6 @@ function SendFees() {
 
   const config = chainsConfig[from?.text];
 
-<<<<<<< HEAD
-    useEffect(() => {
-        if(selectedNFTList.length > 0){ 
-           (async () => {
-            setLoading(true)
-            await estimate();
-            setLoading(false)
-           })()
-        }
-        else setFees("0")
-        const s = setInterval(() => estimate(), 1000 * 30);
-        setEstimateInterval(s)
-        return () => clearInterval(s);
-    }, [selectedNFTList, affiliationFees])
-=======
   useEffect(() => {
     if (selectedNFTList.length > 0) {
       (async () => {
@@ -233,7 +126,6 @@ function SendFees() {
     setEstimateInterval(s);
     return () => clearInterval(s);
   }, [selectedNFTList]);
->>>>>>> new-ui
 
   useEffect(() => {
     clearInterval(estimateInterval);
--- src/components/Wallet/WalletList.jsx
+++ src/components/Wallet/WalletList.jsx
@@ -54,8 +54,6 @@ export default function WalletList({ search, connected, input }) {
       desktop: true,
       order: 3,
     },
-<<<<<<< HEAD
-=======
     {
       Component: (
         <EVMWallet
@@ -70,35 +68,20 @@ export default function WalletList({ search, connected, input }) {
       desktop: biz,
       order: 3,
     },
->>>>>>> new-ui
     {
       Component: (
-        <EVMWallet
-          wallet={"BitKeep"}
-          key="wallet-index-1-bitkeep"
+        <TezosWallet
+          wallet={"TempleWallet"}
+          key="wallet-index-7"
           close={connected}
         />
       ),
-      name: "BitKeep",
-      type: "EVM",
-      mobile: false,
-      desktop: biz,
-      order: 3,
-    } /
-      {
-        Component: (
-          <TezosWallet
-            wallet={"TempleWallet"}
-            key="wallet-index-7"
-            close={connected}
-          />
-        ),
-        name: "Temple Wallet Tezos",
-        type: "Tezos",
-        mobile: true,
-        desktop: true,
-        order: 4,
-      },
+      name: "Temple Wallet Tezos",
+      type: "Tezos",
+      mobile: true,
+      desktop: true,
+      order: 4,
+    },
     {
       Component: (
         <TezosWallet wallet={"Beacon"} key="wallet-index-8" close={connected} />
