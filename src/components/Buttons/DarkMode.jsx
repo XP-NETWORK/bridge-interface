f4725762dd98bfed19dd707110c76431fd1de56d
YuraDXPNetwork <yurad@xp.network>
2022-07-10 14:54:52 +0200

    light mode toggle

--- src/components/Buttons/DarkMode.jsx
+++ src/components/Buttons/DarkMode.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import darkTooggle from "../../assets/img/nav/dark-tooggle.svg";
+import lightToggle from "../../assets/img/nav/light-toggle.svg";
 import { setDarkMode } from "../../store/reducers/generalSlice";
 
 export default function DarkMode() {
     const dispatch = useDispatch();
     const darkMode = useSelector((state) => state.general.darkMode);
-    console.log(
-        "🚀 ~ file: DarkMode.jsx ~ line 8 ~ DarkMode ~ darkMode",
-        darkMode
-    );
-    return (
+
+    return !darkMode ? (
         <div
             onClick={() => dispatch(setDarkMode())}
             className="dark-mode__toggle"
         >
             <img src={darkTooggle} alt="" />
+        </div>
+    ) : (
+        <div
+            onClick={() => dispatch(setDarkMode())}
+            className="light-mode__toggle"
+        >
+            <img src={lightToggle} alt="" />
         </div>
     );
 }
