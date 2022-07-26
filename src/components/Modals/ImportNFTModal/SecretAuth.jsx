e514b28cb3b0bd86fde51c443f26cc710c565131
AlexeyAdoniev <Alexeya@xp.network>
2022-07-26 17:43:11 +0300

    secret auth

--- src/components/Modals/ImportNFTModal/SecretAuth.jsx
+++ src/components/Modals/ImportNFTModal/SecretAuth.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   setImportModal,
   addImportedNFTtoNFTlist,
+  setError,
 } from "../../../store/reducers/generalSlice";
 import { CHAIN_INFO } from "../../values";
 import axios from "axios";
@@ -13,19 +14,20 @@ import CosmosBody from "./CosmosBody";
 import { getFactory } from "../../../wallet/helpers";
 import { Chain } from "xp.network";
 
-const SecretAuth = () => {
+const SecretAuth = ({ nfts }) => {
   const dispatch = useDispatch();
 
   const [importBlocked, setImportBlocked] = useState(false);
-  const [viewKey, setViewKey] = useState();
-  const [contract, setContract] = useState();
-  const [error, setError] = useState("");
+  const [viewKey, setViewKey] = useState("MyViewingKey#1");
+  const [contract, setContract] = useState(
+    "secret1kj69tq5lxlu8vvpjtcltyu58v5476sma4sr9yk"
+  );
+  /// const [error, setError] = useState("");
 
-  const { secretAccount, checkWallet, nfts } = useSelector(
-    ({ general: { secretAccount, checkWallet, NFTList } }) => ({
+  const { secretAccount, checkWallet } = useSelector(
+    ({ general: { secretAccount, checkWallet } }) => ({
       secretAccount,
       checkWallet,
-      nfts: NFTList,
     })
   );
 
@@ -58,6 +60,7 @@ const SecretAuth = () => {
         viewKey,
         contract
       );
+
       if (secretNFTs?.length > 0) {
         secretNFTs.forEach((nft) => {
           if (!isExist(nft)) {
@@ -71,43 +74,58 @@ const SecretAuth = () => {
         });
       }
     } catch (error) {
-      setError(error.message);
+      dispatch(setError(error.message));
       setImportBlocked(false);
-      console.error(error);
+      console.log(error);
     }
   };
 
   return (
-    <div className="secretAuth">
-      <h3>Private ownership </h3>
-      <p>
-        Your assets are protected. Please enter contract address and viewing key
-        below.
-      </p>
-      <div className="fieldsWrapper">
-        <input
-          type="text"
-          placeholder="Paste contract address"
-          value={contract}
-          onChange={(e) => setContract(e.target.value)}
-        />
-        <div className="inputWrapper">
+    <div className="nftListBox">
+      <div className="secretAuth">
+        <h3>Private ownership </h3>
+        <p>
+          Your assets are protected. Please enter contract address and viewing
+          key below.
+        </p>
+        <div className="fieldsWrapper">
           <input
             type="text"
-            placeholder="Enter viewing key"
-            value={viewKey}
-            onChange={(e) => setViewKey(e.target.value)}
+            placeholder="Paste contract address"
+            value={contract}
+            onChange={(e) => setContract(e.target.value)}
           />
+          <div className="inputWrapper">
+            <input
+              type="text"
+              placeholder="Enter viewing key"
+              value={viewKey}
+              onChange={(e) => setViewKey(e.target.value)}
+            />
+          </div>
+        </div>
+        <div
+          className="transfer-button"
+          onClick={fetchSecretNfts}
+          style={!importBlocked ? {} : { opacity: 0.6, pointerEvents: "none" }}
+        >
+          Show assets
         </div>
       </div>
-      <div
-        className="transfer-button"
-        onClick={fetchSecretNfts}
-        style={!importBlocked ? {} : { opacity: 0.6, pointerEvents: "none" }}
-      >
-        Show assets
-      </div>
     </div>
+  );
+};
+
+export const withSecretAuth = (Wrapped) => (props) => {
+  const { from, nfts } = useSelector(({ general: { from, NFTList } }) => ({
+    from,
+    nfts: NFTList,
+  }));
+
+  return !nfts?.length && from.text === "Secret" ? (
+    <SecretAuth nfts={nfts} />
+  ) : (
+    <Wrapped {...props} />
   );
 };
 
--- src/components/NFT/NFTgridView.jsx
+++ src/components/NFT/NFTgridView.jsx
@@ -4,57 +4,52 @@ import NFTempty from "../innercomponents/NFTempty";
 import Missing from "../innercomponents/Missing";
 import BigLoader from "../../components/innercomponents/BigLoader";
 import NFTcard from "./NFTcard";
-import {} from "../../wallet/helpers.js";
+
+import { withSecretAuth } from "../Modals/ImportNFTModal/SecretAuth";
 
 function NFTgridView({ setIndex, scrollIndex }) {
-    const nfts = useSelector((state) => state.general.NFTList);
-    const algorandClaimables = useSelector(
-        (state) => state.general.algorandClaimables
-    );
-    const nftsPlace = window.innerWidth <= 600 ? 2 : 6;
-    const placeholders = new Array(
-        nfts ? (nftsPlace - nfts.length >= 0 ? nftsPlace - nfts.length : 0) : 0
-    ).fill(0);
-    const auto = { overflowX: "auto" };
-    const loader = useSelector((state) => state.general.bigLoader);
+  const nfts = useSelector((state) => state.general.NFTList);
+  const algorandClaimables = useSelector(
+    (state) => state.general.algorandClaimables
+  );
+  const nftsPlace = window.innerWidth <= 600 ? 2 : 6;
+  const placeholders = new Array(
+    nfts ? (nftsPlace - nfts.length >= 0 ? nftsPlace - nfts.length : 0) : 0
+  ).fill(0);
+  const auto = { overflowX: "auto" };
+  const loader = useSelector((state) => state.general.bigLoader);
 
-    return (
-        <div className="nftListBox">
-            {loader ? (
-                <BigLoader />
-            ) : (
-                <div
-                    style={nfts?.length > 0 ? auto : {}}
-                    className="nft-list__wrapper"
-                >
-                    {algorandClaimables &&
-                        algorandClaimables.map((nft, index) => (
-                            <NFTcard
-                                nft={nft}
-                                index={index}
-                                key={`nft-${index}`}
-                                claimables={true}
-                            />
-                        ))}
-                    {nfts?.length
-                        ? nfts?.map((nft, index) => (
-                              <NFTcard
-                                  nft={nft}
-                                  index={index}
-                                  key={`nft-${index}`}
-                              />
-                          ))
-                        : (!algorandClaimables ||
-                              algorandClaimables?.length < 1) && <NFTempty />}
-                    {nfts?.length > 0 && nfts?.length < nftsPlace
-                        ? placeholders.map((n, index) => (
-                              <Missing key={`missing-${index}-component`} />
-                          ))
-                        : ""}
-                </div>
-            )}
+  return (
+    <div className="nftListBox">
+      {loader ? (
+        <BigLoader />
+      ) : (
+        <div style={nfts?.length > 0 ? auto : {}} className="nft-list__wrapper">
+          {algorandClaimables &&
+            algorandClaimables.map((nft, index) => (
+              <NFTcard
+                nft={nft}
+                index={index}
+                key={`nft-${index}`}
+                claimables={true}
+              />
+            ))}
+          {nfts?.length
+            ? nfts?.map((nft, index) => (
+                <NFTcard nft={nft} index={index} key={`nft-${index}`} />
+              ))
+            : (!algorandClaimables || algorandClaimables?.length < 1) && (
+                <NFTempty />
+              )}
+          {nfts?.length > 0 && nfts?.length < nftsPlace
+            ? placeholders.map((n, index) => (
+                <Missing key={`missing-${index}-component`} />
+              ))
+            : ""}
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
-export default NFTgridView;
+export default withSecretAuth(NFTgridView);
