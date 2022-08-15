b9eadcef1308ac74e64b251410cc1e1b835ab60f
YuraDXPNetwork <yurad@xp.network>
2022-08-15 08:50:49 +0300

    visibility styles added

--- src/components/Wallet/HederaWallet.jsx
+++ src/components/Wallet/HederaWallet.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function HederaWallet({ wallet }) {
+    const from = useSelector((state) => state.general.from);
+    const temporaryFrom = useSelector((state) => state.general.temporaryFrom);
+    const OFF = { opacity: 0.6, pointerEvents: "none" };
+
+    const getStyle = () => {
+        switch (true) {
+            case temporaryFrom === "Hedere" || from === "Hedere":
+                return {};
+            case !temporaryFrom && !from:
+                return {};
+            default:
+                return OFF;
+        }
+    };
+
     switch (wallet) {
         case "Hashpack":
             return (
                 <li
-                    // style={getStyle()}
+                    style={getStyle()}
                     // onClick={() => connectHandler("MetaMask")}
                     className="wllListItem"
                     data-wallet="Hashpack"
@@ -17,7 +33,7 @@ export default function HederaWallet({ wallet }) {
         default:
             return (
                 <li
-                    // style={getStyle()}
+                    style={getStyle()}
                     // onClick={() => connectHandler("MetaMask")}
                     className="wllListItem"
                     data-wallet="Blade"
