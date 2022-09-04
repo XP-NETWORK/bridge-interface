3865057a8c50117e3a86adce4966859f974d0208
YuraDXPNetwork <yurad@xp.network>
2022-09-04 15:51:51 +0300

    button search

--- src/components/Buttons/SearchButton.jsx
+++ src/components/Buttons/SearchButton.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import icon from "../../assets/img/icons/Search.svg";
 export default function SearchButton({ handleSearchTop }) {
     return (
         <div onClick={handleSearchTop} className="search-button">
-            <div className="search__icon"></div>
+            <img src={icon} className="search__icon" alt="" />
         </div>
     );
 }
