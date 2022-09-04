a13b720e10c1d78d57e0eec9c84fabe044a36e12
YuraDXPNetwork <yurad@xp.network>
2022-09-04 11:53:37 +0300

    seatch icon fixed

--- src/components/Buttons/SearchButton.jsx
+++ src/components/Buttons/SearchButton.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from "react";
 
-
-export default function SearchButton({handleSearchTop}) {
-  return (
-    <div onClick={handleSearchTop} className='search-button'></div>
-  )
+export default function SearchButton({ handleSearchTop }) {
+    return (
+        <div onClick={handleSearchTop} className="search-button">
+            <div className="search__icon"></div>
+        </div>
+    );
 }
