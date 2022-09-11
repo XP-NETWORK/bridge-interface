4eb3e77a17831ab7da7896be5542c2646cd4f76b
AlexeyAdoniev <Alexeya@xp.network>
2022-09-11 11:25:32 +0300

    fix setError modal bug

--- src/services/chains/evm/evmService.js
+++ src/services/chains/evm/evmService.js
@@ -87,6 +87,7 @@ export const transferNFTFromEVM = async ({
   const fromNonce = CHAIN_INFO[from.text].nonce;
   const toNonce = CHAIN_INFO[to.text].nonce;
   const wrapped = await factory.isWrappedNft(nft, fromNonce);
+
   const {
     native: { contract, tokenId },
     amountToTransfer,
@@ -103,6 +104,7 @@ export const transferNFTFromEVM = async ({
       tokenId && !isNaN(Number(tokenId)) ? tokenId.toString() : undefined
     );
   }
+
   let result;
   switch (true) {
     case to.type === "Cosmos" && !mintWith && !wrapped:
@@ -122,9 +124,10 @@ export const transferNFTFromEVM = async ({
       break;
     case !wrapped && !mintWith && !testnet:
       store.dispatch(
-        setError(
-          "Transfer has been canceled. The NFT you are trying to send will be minted with a default NFT collection"
-        )
+        setError({
+          message:
+            "Transfer has been canceled. The NFT you are trying to send will be minted with a default NFT collection",
+        })
       );
       if (txnHashArr[0]) {
         store.dispatch(setTxnHash({ txn: "failed", nft }));
