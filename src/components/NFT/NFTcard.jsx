43d00076ec0b1b93add7ffe49a0192f1d847fa8d
YuraDXPNetwork <yurad@xp.network>
2022-09-20 14:17:30 +0300

    comming soon and origin chain mark

--- src/components/NFT/NFTcard.jsx
+++ src/components/NFT/NFTcard.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState, useMemo, useRef } from "react";
 import { useDispatch } from "react-redux";
 import {
-  setSelectedNFTList,
-  removeFromSelectedNFTList,
+    setSelectedNFTList,
+    removeFromSelectedNFTList,
 } from "../../store/reducers/generalSlice";
 import NFTdetails from "./NFTdetails";
 import { useSelector } from "react-redux";
-import { isShown } from "./NFTHelper.js";
 import VideoAndImage from "./VideoAndImage";
 import BrockenUtlGridView from "./BrockenUtlGridView";
 import "./NewNFT.css";
@@ -18,157 +17,174 @@ import { parseNFT } from "../../wallet/nftParser";
 import { useDidUpdateEffect } from "../Settings/hooks";
 import Image from "./Image";
 import SFTMark from "./SFTMark";
-import { checkMintWith } from "../../wallet/helpers";
 
 import OnlyVideo from "./OnlyVideo";
+import { chains, chainsConfig } from "../values";
+import OriginChainMark from "./OriginChainMark";
 
 export default function NFTcard({ nft, index, claimables }) {
-  const dispatch = useDispatch();
-  const [detailsOn, setDetailsOn] = useState(false);
-  const search = useSelector((state) => state.general.NFTListSearch);
-  const factory = useSelector((state) => state.general.factory);
-  const from = useSelector((state) => state.general.from);
-  const to = useSelector((state) => state.general.to);
-  const testnet = useSelector((state) => state.general.testNet);
-  const selectedNFTs = useSelector((state) => state.general.selectedNFTList);
-  const [isVisible, setIsVisible] = useState();
-  const localhost = window.location.hostname;
-  const [imageErr, setImageErr] = useState(false);
-  const whitelisted = nft.whitelisted;
-  const [verifiedContract, setVerifiedContract] = useState();
+    const dispatch = useDispatch();
+    const [detailsOn, setDetailsOn] = useState(false);
+    const search = useSelector((state) => state.general.NFTListSearch);
+    const factory = useSelector((state) => state.general.factory);
+    const testnet = useSelector((state) => state.general.testNet);
+    const selectedNFTs = useSelector((state) => state.general.selectedNFTList);
+    const [isVisible, setIsVisible] = useState();
+    const localhost = window.location.hostname;
+    const [imageErr, setImageErr] = useState(false);
 
-  const callBackWhenObserver = (entries) => {
-    const [entry] = entries;
-    setIsVisible(entry.isIntersecting);
-  };
-
-  const cardRef = useRef(null);
-  const options = useMemo(() => {
-    return {
-      root: null,
-      tootMargin: "0px",
-      threshold: 0.3,
+    const callBackWhenObserver = (entries) => {
+        const [entry] = entries;
+        setIsVisible(entry.isIntersecting);
     };
-  }, []);
 
-  let isSelected = selectedNFTs.filter(
-    (n) =>
-      n.native.tokenId === nft.native?.tokenId &&
-      n.native.contract === nft.native?.contract &&
-      n.native.chainId === nft.native?.chainId
-  )[0];
+    const getOriginChain = (originChain) => {
+        // debugger;
+        const _nonce = originChain ? parseInt(originChain) : undefined;
+        const origin = chains.find((e) => e.nonce === _nonce);
+        return chainsConfig[origin?.text]?.img;
+    };
 
-  function addRemoveNFT(chosen) {
-    if (!isSelected) {
-      dispatch(setSelectedNFTList(chosen));
-    } else {
-      dispatch(removeFromSelectedNFTList(nft));
+    const originChainImg = getOriginChain(nft?.originChain);
+
+    const cardRef = useRef(null);
+    const options = useMemo(() => {
+        return {
+            root: null,
+            tootMargin: "0px",
+            threshold: 0.3,
+        };
+    }, []);
+
+    let isSelected = selectedNFTs.filter(
+        (n) =>
+            n.native.tokenId === nft.native?.tokenId &&
+            n.native.contract === nft.native?.contract &&
+            n.native.chainId === nft.native?.chainId
+    )[0];
+
+    function addRemoveNFT(chosen) {
+        if (!isSelected) {
+            dispatch(setSelectedNFTList(chosen));
+        } else {
+            dispatch(removeFromSelectedNFTList(nft));
+        }
     }
-  }
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(callBackWhenObserver, options);
-    const currentTarget = cardRef.current;
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            callBackWhenObserver,
+            options
+        );
+        const currentTarget = cardRef.current;
 
-    if (currentTarget) observer.observe(currentTarget);
-    return () => {
-      if (currentTarget) {
-        observer.unobserve(currentTarget);
-      }
-    };
-  }, [cardRef, options, search]);
-
-  useDidUpdateEffect(() => {
-    if (isVisible) {
-      if (!nft.dataLoaded) {
-        parseNFT(factory)(nft, index, testnet, claimables);
-      }
-    }
-    /* const mw = async () => {
-      const mintWith = await checkMintWith(
-        from,
-        to,
-        nft.native?.contract,
-        nft.native?.tokenId
-      );
-      if (mintWith) setVerifiedContract(true);
-    };
-    if (whitelisted && (from.type === "EVM" || from.type === "Elrond")) {
-      mw();
-    } else setVerifiedContract(true);*/
-  }, [isVisible, nft]);
-
-  return (
-    <>
-      <div className={`nft-box__wrapper`} ref={cardRef}>
-        {!nft?.dataLoaded ? (
-          <Preload />
-        ) : (
-          <div
-            onClick={() =>
-              nft.whitelisted && !detailsOn && !claimables
-                ? addRemoveNFT(nft, index)
-                : undefined
+        if (currentTarget) observer.observe(currentTarget);
+        return () => {
+            if (currentTarget) {
+                observer.unobserve(currentTarget);
             }
-            className={nft.whitelisted ? "nft__card--selected" : "nft__card"}
-          >
-            {nft.native?.amount && <SFTMark amount={nft?.native.amount} />}
-            <div className="nft__main">
-              {nft.uri && nft.image && nft.animation_url ? (
-                <VideoAndImage
-                  index={index}
-                  videoUrl={nft.animation_url}
-                  imageUrl={nft.image}
-                  onError={setImageErr}
-                  nft={nft}
-                />
-              ) : nft.image && !imageErr ? (
-                <Image onError={setImageErr} nft={nft} index={index} />
-              ) : !nft.image && nft.animation_url ? (
-                <OnlyVideo videoUrl={nft.animation_url} />
-              ) : (
-                <BrockenUtlGridView />
-              )}
+        };
+    }, [cardRef, options, search]);
 
-              {!claimables && nft.whitelisted ? (
-                !isSelected ? (
-                  <div className="nft-radio"></div>
+    useDidUpdateEffect(() => {
+        if (isVisible) {
+            if (!nft.dataLoaded) {
+                parseNFT(factory)(nft, index, testnet, claimables);
+            }
+        }
+    }, [isVisible, nft]);
+
+    return (
+        <>
+            <div className={`nft-box__wrapper`} ref={cardRef}>
+                {!nft?.dataLoaded ? (
+                    <Preload />
                 ) : (
-                  <div className="nft-radio--selected"></div>
-                )
-              ) : (
-                ""
-              )}
+                    <div
+                        onClick={() =>
+                            nft.whitelisted && !detailsOn && !claimables
+                                ? addRemoveNFT(nft, index)
+                                : undefined
+                        }
+                        className={
+                            nft.whitelisted
+                                ? "nft__card--selected"
+                                : "nft__card"
+                        }
+                    >
+                        {nft.native?.amount && (
+                            <SFTMark amount={nft?.native.amount} />
+                        )}
+                        {originChainImg && (
+                            <OriginChainMark icon={originChainImg} />
+                        )}
+                        <div className="nft__main">
+                            {nft.uri && nft.image && nft.animation_url ? (
+                                <VideoAndImage
+                                    index={index}
+                                    videoUrl={nft.animation_url}
+                                    imageUrl={nft.image}
+                                    onError={setImageErr}
+                                    nft={nft}
+                                />
+                            ) : nft.image && !imageErr ? (
+                                <Image
+                                    onError={setImageErr}
+                                    nft={nft}
+                                    index={index}
+                                />
+                            ) : !nft.image && nft.animation_url ? (
+                                <OnlyVideo videoUrl={nft.animation_url} />
+                            ) : (
+                                <BrockenUtlGridView />
+                            )}
 
-              {!nft.whitelisted /*|| !verifiedContract*/ && <NotWhiteListed />}
-              {claimables && <ClaimableCard nft={nft} index={index} />}
+                            {!claimables && nft.whitelisted ? (
+                                !isSelected ? (
+                                    <div className="nft-radio"></div>
+                                ) : (
+                                    <div className="nft-radio--selected"></div>
+                                )
+                            ) : (
+                                ""
+                            )}
+
+                            {!nft.whitelisted /*|| !verifiedContract*/ && (
+                                <NotWhiteListed />
+                            )}
+                            {claimables && (
+                                <ClaimableCard nft={nft} index={index} />
+                            )}
+                        </div>
+                        <div className="nft__footer">
+                            {localhost === "localhost" && (
+                                <span
+                                    style={{
+                                        fontSize: "10px",
+                                        color: "red",
+                                    }}
+                                >
+                                    index: {index}
+                                </span>
+                            )}
+                            <span className="nft-name">
+                                <span className="name">
+                                    {nft.name || nft.native?.name}
+                                </span>
+                                <NFTdetails
+                                    details={setDetailsOn}
+                                    nftInf={nft}
+                                    index={index}
+                                    claimables={claimables}
+                                />
+                            </span>
+                            <span className="nft-number">
+                                {nft.native?.tokenId}
+                            </span>
+                        </div>
+                    </div>
+                )}
             </div>
-            {/* // ! */}
-            <div className="nft__footer">
-              {localhost === "localhost" && (
-                <span
-                  style={{
-                    fontSize: "10px",
-                    color: "red",
-                  }}
-                >
-                  index: {index}
-                </span>
-              )}
-              <span className="nft-name">
-                <span className="name">{nft.name || nft.native?.name}</span>
-                <NFTdetails
-                  details={setDetailsOn}
-                  nftInf={nft}
-                  index={index}
-                  claimables={claimables}
-                />
-              </span>
-              <span className="nft-number">{nft.native?.tokenId}</span>
-            </div>
-          </div>
-        )}
-      </div>
-    </>
-  );
+        </>
+    );
 }
--- src/services/cacheService.js
+++ src/services/cacheService.js
@@ -1,121 +1,123 @@
 import axios from "axios";
 
 class CacheService {
-  cacheApi = "https://nft-cache.herokuapp.com"; //"https://nft-cache-testing.herokuapp.com"; //"http://localhost:3030"; //"https://nft-cache.herokuapp.com";
-  retryInterval = 6000;
-  totalTry = 6;
-  retryStatues = [429];
-  forceCache = ["nft.weedcommerce.info"];
+    cacheApi = "https://nft-cache.herokuapp.com"; //"https://nft-cache-testing.herokuapp.com"; //"http://localhost:3030"; //"https://nft-cache.herokuapp.com";
+    retryInterval = 6000;
+    totalTry = 6;
+    retryStatues = [429];
+    forceCache = ["nft.weedcommerce.info"];
 
-  constructor() {
-    this.axios = axios.create({
-      baseURL: this.widgetApi,
-      headers: {
-        "Content-type": "application/json",
-      },
-      timeout: 5000,
-    });
-  }
-
-  async get({ chainId, tokenId, contract }, nft) {
-    try {
-      return axios
-        .get(
-          `${this.cacheApi}/nft/data?chainId=${chainId ||
-            nft.native?.chainId}&tokenId=${tokenId ||
-            nft.native?.tokenId}&contract=${encodeURIComponent(contract) ||
-            encodeURIComponent(nft.native?.contract)}`,
-          {
+    constructor() {
+        this.axios = axios.create({
+            baseURL: this.widgetApi,
+            headers: {
+                "Content-type": "application/json",
+            },
             timeout: 5000,
-          }
-        )
-        .catch(() => ({ data: null }));
-    } catch (e) {
-      console.log(e);
+        });
     }
-  }
 
-  async add(nft, account, whitelisted, times = 1) {
-    return axios
-      .post(`${this.cacheApi}/nft/add`, {
-        nft,
-        account,
-        whitelisted,
-      })
-      .then(async (res) => {
-        if (
-          this.retryStatues.includes(res.data?.errorStatus) &&
-          times < this.totalTry
-        ) {
-          await new Promise((resolve) =>
-            setTimeout(
-              () => resolve(""),
-              this.retryInterval + (this.retryInterval * times) / 5
-            )
-          );
-          return this.add(nft, account, whitelisted, times++);
-        } else {
-          return res.data;
+    async get({ chainId, tokenId, contract }, nft) {
+        try {
+            return axios
+                .get(
+                    `${this.cacheApi}/nft/data?chainId=${chainId ||
+                        nft.native?.chainId}&tokenId=${tokenId ||
+                        nft.native?.tokenId}&contract=${encodeURIComponent(
+                        contract
+                    ) || encodeURIComponent(nft.native?.contract)}`,
+                    {
+                        timeout: 5000,
+                    }
+                )
+                .catch(() => ({ data: null }));
+        } catch (e) {
+            console.log(e);
         }
-      });
-  }
+    }
 
-  async unwrap(nft) {
-    if (/(wnfts\.xp\.network|nft\.xp\.network)/.test(nft.uri)) {
-      try {
-        const res = await axios(nft.uri);
+    async add(nft, account, whitelisted, times = 1) {
+        return axios
+            .post(`${this.cacheApi}/nft/add`, {
+                nft,
+                account,
+                whitelisted,
+            })
+            .then(async (res) => {
+                if (
+                    this.retryStatues.includes(res.data?.errorStatus) &&
+                    times < this.totalTry
+                ) {
+                    await new Promise((resolve) =>
+                        setTimeout(
+                            () => resolve(""),
+                            this.retryInterval +
+                                (this.retryInterval * times) / 5
+                        )
+                    );
+                    return this.add(nft, account, whitelisted, times++);
+                } else {
+                    return res.data;
+                }
+            });
+    }
 
-        const { data } = res;
+    async unwrap(nft) {
+        if (/(wnfts\.xp\.network|nft\.xp\.network)/.test(nft.uri)) {
+            try {
+                const res = await axios(nft.uri);
 
-        let tokenId = data.wrapped?.token_id || data.wrapped?.tokenId;
-        const sourceToken = data.wrapped?.source_token_id;
+                const { data } = res;
 
-        if (
-          data.wrapped?.origin === "2" &&
-          tokenId.split("-").length < 3 &&
-          sourceToken
-        ) {
-          tokenId = tokenId + `-${sourceToken}`;
+                let tokenId = data.wrapped?.token_id || data.wrapped?.tokenId;
+                const sourceToken = data.wrapped?.source_token_id;
+
+                if (
+                    data.wrapped?.origin === "2" &&
+                    tokenId.split("-").length < 3 &&
+                    sourceToken
+                ) {
+                    tokenId = tokenId + `-${sourceToken}`;
+                }
+                const contract =
+                    data.wrapped?.contract || data.wrapped?.source_mint_ident;
+
+                return {
+                    nft: {
+                        ...nft,
+                        collectionIdent: contract,
+                        uri: data.wrapped?.original_uri,
+                        native: {
+                            ...nft.native,
+                            chainId: data.wrapped?.origin,
+                            contract,
+                            tokenId,
+                            uri: data.wrapped?.original_uri,
+                        },
+                    },
+                    chainId: data.wrapped?.origin,
+                    tokenId,
+                    contract,
+                };
+            } catch (e) {}
         }
-        const contract =
-          data.wrapped?.contract || data.wrapped?.source_mint_ident;
 
         return {
-          nft: {
-            ...nft,
-            collectionIdent: contract,
-            uri: data.wrapped?.original_uri,
-            native: {
-              ...nft.native,
-              chainId: data.wrapped?.origin,
-              contract,
-              tokenId,
-              uri: data.wrapped?.original_uri,
-            },
-          },
-          chainId: data.wrapped?.origin,
-          tokenId,
-          contract,
+            nft,
+            chainId: nft.native?.chainId,
+            tokenId: nft.native?.tokenId,
+            contract: nft.collectionIdent,
         };
-      } catch (e) {}
     }
 
-    return {
-      nft,
-      chainId: nft.native?.chainId,
-      tokenId: nft.native?.tokenId,
-      contract: nft.collectionIdent,
-    };
-  }
+    isRestricted = (url) => this.forceCache.some((r) => url?.includes(r));
 
-  isRestricted = (url) => this.forceCache.some((r) => url?.includes(r));
-
-  preventRestricted = (nft) => ({
-    ...nft,
-    image: "",
-    animation_url: "",
-    uri: "",
-  });
+    preventRestricted = (nft) => ({
+        ...nft,
+        image: "",
+        animation_url: "",
+        uri: "",
+    });
 }
 
 export default () => new CacheService();
