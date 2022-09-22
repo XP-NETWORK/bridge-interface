5ab08a08cf239ba0cea4adbfba9db2718655b31e
YuraDXPNetwork <yurad@xp.network>
2022-09-22 11:47:56 +0300

    audited by stickers

---
+++ src/components/innercomponents/AuditedBy.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import certik from "../../assets/img/audited_by/certik.svg";
+import hacken from "../../assets/img/audited_by/hacken.svg";
+import smart_state from "../../assets/img/audited_by/smart_state.svg";
+import safepress from "../../assets/img/audited_by/safepress.svg";
+import "./AuditedBy.css";
+export default function AuditedBy() {
+    return (
+        <div className="audited__wrapper">
+            <div className="audited">
+                <div className="audited__title">Audited by: </div>
+                <div className="audited__stickers">
+                    <img src={certik} alt="Certik" />
+                    <img src={hacken} alt="Hacken" />
+                    <img src={smart_state} alt="Smart state" />
+                    <img src={safepress} alt="Safepress" />
+                </div>
+            </div>
+        </div>
+    );
+}
