5ea10397f6417330b00884dc273a5222d0129fb6
AlexeyAdoniev <Alexeya@xp.network>
2022-11-08 13:33:46 +0200

    merge

--- src/services/chains/ton/tonHelper.js
+++ src/services/chains/ton/tonHelper.js
@@ -6,72 +6,80 @@ import { setQRCodeModal } from "../../../components/Wallet/TONWallet/tonStore";
 import BigNumber from "bignumber.js";
 
 export const transferNFTFromTON = async ({
-    to,
-    from,
-    nft,
-    signer,
-    receiver,
-    fee,
+  to,
+  from,
+  nft,
+  signer,
+  receiver,
+  fee,
 }) => {
-    // eslint-disable-next-line no-debugger
+  console.log("🚀 ~ file: tonHelper.js ~ line 15 ~ signer", signer);
+  // eslint-disable-next-line no-debugger
 
-    const {
-        general: { factory },
-    } = store.getState();
-    const toChain = await factory.inner(chainsConfig[to.text].Chain);
-    const fromChain = await factory.inner(chainsConfig[from.text].Chain);
+  const {
+    general: { factory },
+  } = store.getState();
+  const toChain = await factory.inner(chainsConfig[to.text].Chain);
+  const fromChain = await factory.inner(chainsConfig[from.text].Chain);
+  // const fromNonce = CHAIN_INFO[from.text].nonce;
+  // const toNonce = CHAIN_INFO[to.text].nonce;
+  // const wrapped = await factory.isWrappedNft(nft, fromNonce);
 
-    const {
-        general: { tonAccount },
-    } = store.getState();
-    let mintWith;
+  // const {
+  //     // native: { contract, tokenId },
+  //     amountToTransfer,
+  // } = nft;
+  const {
+    general: { tonAccount },
+  } = store.getState();
+  let mintWith;
+  // if (!wrapped) {
+  //     mintWith = await factory.getVerifiedContract(
+  //         contract,
+  //         toNonce,
+  //         fromNonce,
+  //         tokenId && !isNaN(Number(tokenId)) ? tokenId.toString() : undefined
+  //     );
+  // }
 
-    let result;
+  let result;
 
-    switch (true) {
-        default:
-            result = await transfer(
-                fromChain,
-                toChain,
-                nft,
-                signer,
-                receiver,
-                undefined,
-                fee,
-                mintWith,
-                factory,
-                tonAccount
-            );
-            break;
-    }
-    return result || false;
+  switch (true) {
+    default:
+      result = await transfer(
+        fromChain,
+        toChain,
+        nft,
+        signer,
+        receiver,
+        undefined,
+        fee,
+        mintWith,
+        factory,
+        tonAccount
+      );
+      break;
+  }
+  return result || false;
 };
 
 const transfer = async (
-    fromChain,
-    toChain,
-    nft,
-    signer,
-    receiver,
-    amount,
-    fee,
-    mintWith,
-    factory,
-    account
+  fromChain,
+  toChain,
+  nft,
+  signer,
+  receiver,
+  amount,
+  fee,
+  mintWith,
+  factory,
+  account
 ) => {
-    let result;
-    const {
-        general: { from, to },
-    } = store.getState();
+  let result;
+  const {
+    general: { from, to },
+  } = store.getState();
 
-<<<<<<< HEAD
-    nft = {
-        ...nft,
-        native: {
-            ...nft.native,
-            nftItemAddr: nft.native.address,
-        },
-=======
   nft = {
     ...nft,
     native: {
@@ -117,49 +125,9 @@ const transfer = async (
       toChain: to.text,
       message: error,
       nfts: nft.native,
->>>>>>> ebd91172 (dsa)
     };
+    errorToLog(errBogy);
+  }
 
-    try {
-        switch (true) {
-            case amount > 0:
-                result = await factory.transferSft(
-                    fromChain,
-                    toChain,
-                    nft,
-                    signer,
-                    receiver,
-                    new BigNumber(amount),
-                    fee,
-                    mintWith
-                );
-                return result;
-            default:
-                result = await factory.transferNft(
-                    fromChain,
-                    toChain,
-                    nft,
-                    signer,
-                    receiver,
-                    new BigNumber(fee),
-                    mintWith
-                );
-                return result;
-        }
-    } catch (error) {
-        store.dispatch(setError(error));
-        const date = new Date();
-        const errBogy = {
-            type: "Transfer",
-            walletAddress: account,
-            time: date.toString(),
-            fromChain: from.text,
-            toChain: to.text,
-            message: error,
-            nfts: nft.native,
-        };
-        errorToLog(errBogy);
-    }
-
-    store.dispatch(setQRCodeModal(false));
+  store.dispatch(setQRCodeModal(false));
 };
