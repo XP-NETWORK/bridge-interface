fd02580ee7354bd414f13531e00d2353821c5587
YuraDXPNetwork <yurad@xp.network>
2022-12-15 11:18:57 +0200

    :pull:

--- src/components/NFTsBoard/NFTaccount.jsx
+++ src/components/NFTsBoard/NFTaccount.jsx
@@ -5,12 +5,12 @@ import { Container } from "react-bootstrap";
 import { Modal } from "react-bootstrap";
 import ImportNFTModal from "../Modals/ImportNFTModal/ImportNFTModal";
 import {
-  setBalance,
-  setError,
-  cleanSelectedNFTList,
-  setBigLoader,
-  setPreloadNFTs,
-  setNFTList,
+    setBalance,
+    setError,
+    cleanSelectedNFTList,
+    setBigLoader,
+    setPreloadNFTs,
+    setNFTList,
 } from "../../store/reducers/generalSlice";
 import { setIsEmpty } from "../../store/reducers/paginationSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -24,9 +24,9 @@ import SelectNFTAler from "../Alerts/SelectNFTAler";
 import PasteDestinationAlert from "../Alerts/PasteDestinationAlert";
 import NoApprovedNFT from "../Alerts/NoApprovedNFT";
 import {
-  usePrevious,
-  useCheckMobileScreen,
-  useDidUpdateEffect,
+    usePrevious,
+    useCheckMobileScreen,
+    useDidUpdateEffect,
 } from "../Settings/hooks";
 
 import WalletConnectionModal from "../Wallet/WalletConnectionModal";
@@ -44,179 +44,188 @@ import withChains from "./hocs";
 const intervalTm = 10_000;
 
 function NFTaccount(props) {
-  const { serviceContainer, chainSpecific, _from, algorandAccount } = props;
+    const { serviceContainer, chainSpecific, _from, algorandAccount } = props;
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const from = _from.key;
-  const prevSelected = usePrevious(from);
+    const from = _from.key;
+    const prevSelected = usePrevious(from);
 
-  const nfts = useSelector((state) => state.general.NFTList);
-  const currentsNFTs = useSelector((state) => state.general.currentsNFTs);
+    const nfts = useSelector((state) => state.general.NFTList);
+    const currentsNFTs = useSelector((state) => state.general.currentsNFTs);
 
-  const importModal = useSelector((state) => state.general.importModal);
+    const importModal = useSelector((state) => state.general.importModal);
 
-  const tronWallet = useSelector((state) => state.general.tronWallet);
-  const account = useSelector((state) => state.general.account);
+    const tronWallet = useSelector((state) => state.general.tronWallet);
+    const account = useSelector((state) => state.general.account);
 
-  const prevAccount = usePrevious(account);
-  const tezosAccount = useSelector((state) => state.general.tezosAccount);
-  const elrondAccount = useSelector((state) => state.general.elrondAccount);
-  const hederaAccount = useSelector((state) => state.general.hederaAccount);
-  const secretAccount = useSelector((state) => state.general.secretAccount);
-  const tonAccount = useSelector((state) => state.general.tonAccount);
-  const NFTSetToggler = useSelector((state) => state.general.NFTSetToggler);
-  const prevNFTSetToggler = usePrevious(NFTSetToggler);
+    const prevAccount = usePrevious(account);
+    const tezosAccount = useSelector((state) => state.general.tezosAccount);
+    const elrondAccount = useSelector((state) => state.general.elrondAccount);
+    const hederaAccount = useSelector((state) => state.general.hederaAccount);
+    const secretAccount = useSelector((state) => state.general.secretAccount);
+    const tonAccount = useSelector((state) => state.general.tonAccount);
+    const NFTSetToggler = useSelector((state) => state.general.NFTSetToggler);
+    const prevNFTSetToggler = usePrevious(NFTSetToggler);
 
-  const selectedNFTs = useSelector((state) => state.general.selectedNFTList);
+    const selectedNFTs = useSelector((state) => state.general.selectedNFTList);
 
-  const unwrappedEGold = useSelector((state) => state.general.unwrappedEGold);
+    const unwrappedEGold = useSelector((state) => state.general.unwrappedEGold);
 
-  const checkWallet = useSelector((state) => state.general.checkWallet);
+    const checkWallet = useSelector((state) => state.general.checkWallet);
 
-  const accountWalletModal = useSelector(
-    (state) => state.general.accountWalletModal
-  );
+    const accountWalletModal = useSelector(
+        (state) => state.general.accountWalletModal
+    );
 
-  let _account =
-    checkWallet ||
-    hederaAccount ||
-    account ||
-    algorandAccount ||
-    tezosAccount ||
-    elrondAccount ||
-    tronWallet ||
-    secretAccount ||
-    tonAccount;
+    let _account =
+        checkWallet ||
+        hederaAccount ||
+        account ||
+        algorandAccount ||
+        tezosAccount ||
+        elrondAccount ||
+        tronWallet ||
+        secretAccount ||
+        tonAccount;
 
-  const { bridge } = serviceContainer;
+    const { bridge } = serviceContainer;
 
-  async function getNFTsList(fromChain) {
-    dispatch(setBigLoader(true));
-    try {
-      let nfts = await fromChain.getNFTs(bridge.checkWallet || _account);
-      nfts = fromChain.filterNFTs(nfts);
-      if (nfts.length < 1) {
-        dispatch(setIsEmpty(true));
-      } else {
-        dispatch(setIsEmpty(false));
-        dispatch(setNFTList(nfts));
-        dispatch(setPreloadNFTs(nfts.length));
-      }
+    async function getNFTsList(fromChain) {
+        dispatch(setBigLoader(true));
+        try {
+            let nfts = await fromChain.getNFTs(bridge.checkWallet || _account);
+            nfts = fromChain.filterNFTs(nfts);
+            if (nfts.length < 1) {
+                dispatch(setIsEmpty(true));
+            } else {
+                dispatch(setIsEmpty(false));
+                dispatch(setNFTList(nfts));
+                dispatch(setPreloadNFTs(nfts.length));
+            }
 
-      dispatch(setBigLoader(false));
-    } catch (error) {
-      console.log(error);
-      dispatch(setBigLoader(false));
-      dispatch(setNFTList([]));
-      console.log(error);
-      dispatch(setError(error.data ? error.data.message : error.message));
+            dispatch(setBigLoader(false));
+        } catch (error) {
+            console.log(error);
+            dispatch(setBigLoader(false));
+            dispatch(setNFTList([]));
+            console.log(error);
+            dispatch(setError(error.data ? error.data.message : error.message));
+        }
     }
-  }
 
-  const getBalance = async (fromChain) => {
-    const _balance = await fromChain.balance(_account);
-    dispatch(setBalance(_balance));
-  };
-
-  useDidUpdateEffect(() => {
-    const checkLocked = async () => {
-      const data = await checkXpNetLocked(account);
-      dispatch(setDiscountLeftUsd(Math.round(data?.discountLeftUsd / 0.25)));
+    const getBalance = async (fromChain) => {
+        const _balance = await fromChain.balance(_account);
+        dispatch(setBalance(_balance));
     };
-    account && checkLocked();
-  }, [account]);
 
-  useDidUpdateEffect(() => {
-    if (nfts.length) {
-      saveForSearch(_account, _from.nonce, nfts);
-    }
-  }, [nfts]);
+    useDidUpdateEffect(() => {
+        const checkLocked = async () => {
+            const data = await checkXpNetLocked(account);
+            dispatch(
+                setDiscountLeftUsd(Math.round(data?.discountLeftUsd / 0.25))
+            );
+        };
+        account && checkLocked();
+    }, [account]);
 
-  useEffect(() => {
-    dispatch(cleanSelectedNFTList());
-    let balanceInterval;
-    (async () => {
-      const fromChain = await bridge.getChain(_from.nonce);
-
-      //load nfts
-      _account &&
-        (prevSelected !== _from.key ||
-          prevAccount !== _account ||
-          NFTSetToggler !== prevNFTSetToggler) &&
-        getNFTsList(fromChain);
-
-      //update Balance
-      getBalance(fromChain);
-      chainSpecific && chainSpecific(dispatch, fromChain, _account);
-      balanceInterval = setInterval(() => getBalance(fromChain), intervalTm);
-      console.log(fromChain);
-      const keyHandler = async (event) => {
-        if (event.isComposing || event.keyCode === 229) {
-          return;
+    useDidUpdateEffect(() => {
+        if (nfts.length) {
+            saveForSearch(_account, _from.nonce, nfts);
         }
-        if (event.key === "4") {
-          fromChain.mintNFT("https://meta.polkamon.com/meta?id=10001852306");
-        }
-      };
+    }, [nfts]);
 
-      window.addEventListener("keydown", keyHandler);
-    })();
+    useEffect(() => {
+        dispatch(cleanSelectedNFTList());
+        let balanceInterval;
+        (async () => {
+            const fromChain = await bridge.getChain(_from.nonce);
 
-    return () => clearInterval(balanceInterval);
-  }, [_from, _account, NFTSetToggler]);
+            //load nfts
+            _account &&
+                (prevSelected !== _from.key ||
+                    prevAccount !== _account ||
+                    NFTSetToggler !== prevNFTSetToggler) &&
+                getNFTsList(fromChain);
 
-  const isMobile = useCheckMobileScreen();
+            //update Balance
+            getBalance(fromChain);
+            chainSpecific && chainSpecific(dispatch, fromChain, _account);
+            balanceInterval = setInterval(
+                () => getBalance(fromChain),
+                intervalTm
+            );
+            console.log(fromChain);
+            const keyHandler = async (event) => {
+                // eslint-disable-next-line no-debugger
+                debugger;
+                if (event.isComposing || event.keyCode === 229) {
+                    return;
+                }
+                if (event.key === "4") {
+                    fromChain.mintNFT(
+                        "https://meta.polkamon.com/meta?id=10001852306"
+                    );
+                }
+            };
 
-  return (
-    <div className="NFTaccount">
-      <Modal
-        show={importModal}
-        animation={null}
-        className=" ChainModal import-nft__modal"
-      >
-        <ImportNFTModal />
-      </Modal>
-      <Modal
-        show={accountWalletModal}
-        // onHide={handleClose}
-        animation={null}
-        className="ChainModal wallet-modal"
-      >
-        <WalletConnectionModal />
-      </Modal>
-      <Modal
-        show={unwrappedEGold}
-        animation={null}
-        className="eGold-success ChainModal"
-      >
-        <EGoldSuccess />
-      </Modal>
-      <ChangeNetworkModal />
-      <ChangeWalletModal />
-      <UnsupportedNetwork />
-      <SelectNFTAler />
-      <PasteDestinationAlert />
-      <NoApprovedNFT />
-      <Container className="nftSlectContaine">
-        <ReturnBtn />
-        <div className="row">
-          <div className="nftListCol col-lg-8">
-            {!isMobile && <NFTscreen />}
-            {/*isMobile && <MobileTransferBoard />*/}
-          </div>
-          {!isMobile && <DesktopTransferBoard />}
+            window.addEventListener("keydown", keyHandler);
+        })();
+
+        return () => clearInterval(balanceInterval);
+    }, [_from, _account, NFTSetToggler]);
+
+    const isMobile = useCheckMobileScreen();
+
+    return (
+        <div className="NFTaccount">
+            <Modal
+                show={importModal}
+                animation={null}
+                className=" ChainModal import-nft__modal"
+            >
+                <ImportNFTModal />
+            </Modal>
+            <Modal
+                show={accountWalletModal}
+                // onHide={handleClose}
+                animation={null}
+                className="ChainModal wallet-modal"
+            >
+                <WalletConnectionModal />
+            </Modal>
+            <Modal
+                show={unwrappedEGold}
+                animation={null}
+                className="eGold-success ChainModal"
+            >
+                <EGoldSuccess />
+            </Modal>
+            <ChangeNetworkModal />
+            <ChangeWalletModal />
+            <UnsupportedNetwork />
+            <SelectNFTAler />
+            <PasteDestinationAlert />
+            <NoApprovedNFT />
+            <Container className="nftSlectContaine">
+                <ReturnBtn />
+                <div className="row">
+                    <div className="nftListCol col-lg-8">
+                        {!isMobile && <NFTscreen />}
+                        {/*isMobile && <MobileTransferBoard />*/}
+                    </div>
+                    {!isMobile && <DesktopTransferBoard />}
+                </div>
+                {isMobile && (
+                    <NFTmobileView
+                        selectedNFTs={selectedNFTs}
+                        _from={_from}
+                        nfts={currentsNFTs}
+                    />
+                )}
+            </Container>
         </div>
-        {isMobile && (
-          <NFTmobileView
-            selectedNFTs={selectedNFTs}
-            _from={_from}
-            nfts={currentsNFTs}
-          />
-        )}
-      </Container>
-    </div>
-  );
+    );
 }
 
 export default withChains(NFTaccount);
