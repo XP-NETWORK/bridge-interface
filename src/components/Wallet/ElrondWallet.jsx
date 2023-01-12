d8c93d630384cd3bf414015b6340f44233735d9f
AlexeyAdoniev <Alexeya@xp.network>
2023-01-12 16:34:24 +0200

    elronds wallet merge

--- src/components/Wallet/ElrondWallet.jsx
+++ src/components/Wallet/ElrondWallet.jsx
@@ -4,13 +4,13 @@ import Elrond from "../../assets/img/wallet/Elrond.svg";
 import Maiar from "../../assets/img/wallet/Maiar.svg";
 
 import {
-    setAccount,
-    setError,
-    setQrCodeString,
-    setConfirmMaiarMob,
-    setOnMaiar,
-    setQrImage,
-    setFrom,
+  setAccount,
+  setError,
+  setQrCodeString,
+  setConfirmMaiarMob,
+  setOnMaiar,
+  setQrImage,
+  setFrom,
 } from "../../store/reducers/generalSlice";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -19,116 +19,95 @@ import { withServices } from "../App/hocs/withServices";
 import { Chain } from "xp.network";
 import QRCode from "qrcode";
 import {
-    WalletConnectProvider,
-    ProxyProvider,
-    ExtensionProvider,
+  WalletConnectProvider,
+  ProxyProvider,
+  ExtensionProvider,
 } from "@elrondnetwork/erdjs";
 
-
-import { getRightPath } from "../../wallet/helpers";
 import { chains, getChainObject } from "../../components/values";
+import { getRightPath } from "../../wallet/helpers";
 
 function ElrondWallet({ wallet, close, serviceContainer }) {
-    const { bridge } = serviceContainer;
-    const OFF = { opacity: 0.6, pointerEvents: "none" };
-    const from = useSelector((state) => state.general.from);
-    const temporaryFrom = useSelector((state) => state.general.temporaryFrom);
-    const to = useSelector((state) => state.general.to);
-    // const testnet = useSelector((state) => state.general.testNet);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const { bridge } = serviceContainer;
+  const OFF = { opacity: 0.6, pointerEvents: "none" };
+  const from = useSelector((state) => state.general.from);
+  const temporaryFrom = useSelector((state) => state.general.temporaryFrom);
+  const to = useSelector((state) => state.general.to);
+  // const testnet = useSelector((state) => state.general.testNet);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    // const query = window.location.search || "";
+  // const query = window.location.search || "";
 
-    const handleConnect = async (wallet) => {
-        try {
-            const chainWrapper = await bridge.getChain(
-                from?.nonce || Chain.ELROND
-            );
+  const handleConnect = async (wallet) => {
+    try {
+      const chainWrapper = await bridge.getChain(from?.nonce || Chain.ELROND);
 
-            let account = {};
+      let account = {};
 
-            switch (wallet) {
-                case "Maiar": {
-                    await new Promise((r) => {
-                        (async () => {
-                            const provider = new ProxyProvider(
-                                "https://gateway.elrond.com"
-                            );
-                            const maiarProvider = new WalletConnectProvider(
-                                provider,
-                                "https://bridge.walletconnect.org/"
-                            );
-                            await maiarProvider.init();
+      switch (wallet) {
+        case "Maiar": {
+          await new Promise((r) => {
+            (async () => {
+              const provider = new ProxyProvider("https://gateway.elrond.com");
+              const maiarProvider = new WalletConnectProvider(
+                provider,
+                "https://bridge.walletconnect.org/"
+              );
+              await maiarProvider.init();
 
-                            maiarProvider.onClientConnect = {
-                                onClientLogin: async () => {
-                                    const addess = await maiarProvider.getAddress();
-                                    account.address = addess;
-                                    account.signer = maiarProvider;
-                                    dispatch(setConfirmMaiarMob(true));
-                                    dispatch(setOnMaiar(true));
-                                    dispatch(setQrImage(null));
-                                    !from &&
-                                        dispatch(
-                                            setFrom(
-                                                chains.find(
-                                                    (chain) =>
-                                                        chain.nonce ===
-                                                        Chain.ELROND
-                                                )
-                                            )
-                                        );
-                                    r();
-                                },
-                                onClientLogout: async () => {
-                                    chainWrapper.setSigner(null);
-                                    dispatch(
-                                        setError({
-                                            message:
-                                                "You have disconnected from Maiar, in order to transfer assets please login again",
-                                        })
-                                    );
-                                },
-                            };
+              maiarProvider.onClientConnect = {
+                onClientLogin: async () => {
+                  const addess = await maiarProvider.getAddress();
+                  account.address = addess;
+                  account.signer = maiarProvider;
+                  dispatch(setConfirmMaiarMob(true));
+                  dispatch(setOnMaiar(true));
+                  dispatch(setQrImage(null));
+                  !from &&
+                    dispatch(
+                      setFrom(
+                        chains.find((chain) => chain.nonce === Chain.ELROND)
+                      )
+                    );
+                  r();
+                },
+                onClientLogout: async () => {
+                  chainWrapper.setSigner(null);
+                  dispatch(
+                    setError({
+                      message:
+                        "You have disconnected from Maiar, in order to transfer assets please login again",
+                    })
+                  );
+                },
+              };
 
-                            const qrCodeString = await maiarProvider.login();
-                            dispatch(setQrCodeString(qrCodeString));
-                            const qr = await QRCode.toDataURL(qrCodeString);
-                            dispatch(setQrImage(qr));
-                        })();
-                    });
-                    break;
-                }
-                case "Maiar Extension": {
-                    const instance = ExtensionProvider.getInstance();
-                    await instance
-                        .init()
-                        .catch(() =>
-                            window.open("https://getmaiar.com/defi", "_blank")
-                        );
-                    await instance.login();
-                    const {
-                        account: { address },
-                    } = instance;
-                    if (account?.name === "CanceledError") {
-                        throw new Error("CanceledError");
-                    }
-                    account.address = address;
-                    account.signer = instance;
-                    break;
-                }
-            }
-
-            dispatch(setAccount(account.address));
-            chainWrapper.setSigner(account.signer);
-            bridge.setCurrentType(chainWrapper);
-            close();
-            if (to) navigateToAccountRoute();
-        } catch (e) {
-            dispatch(setError(e));
+              const qrCodeString = await maiarProvider.login();
+              dispatch(setQrCodeString(qrCodeString));
+              const qr = await QRCode.toDataURL(qrCodeString);
+              dispatch(setQrImage(qr));
+            })();
+          });
+          break;
         }
-    };
+        case "Maiar Extension": {
+          const instance = ExtensionProvider.getInstance();
+          await instance
+            .init()
+            .catch(() => window.open("https://getmaiar.com/defi", "_blank"));
+          await instance.login();
+          const {
+            account: { address },
+          } = instance;
+          if (account?.name === "CanceledError") {
+            throw new Error("CanceledError");
+          }
+          account.address = address;
+          account.signer = instance;
+          break;
+        }
+      }
 
       dispatch(setAccount(account.address));
       chainWrapper.setSigner(account.signer);
@@ -138,47 +117,53 @@ function ElrondWallet({ wallet, close, serviceContainer }) {
       if (!from) {
         dispatch(setFrom(getChainObject(Chain.ELROND)));
       }
- 
+    } catch (e) {
+      dispatch(setError(e));
+    }
   };
 
-    const getStyle = () => {
-        if (temporaryFrom?.type === "Elrond") {
-            return {};
-        } else if (temporaryFrom && temporaryFrom?.type !== "Elrond") {
-            return OFF;
-        } else if (!from) {
-            return {};
-        } else if (from && from.text === "Elrond") {
-            return {};
-        } else return OFF;
-    };
+  const navigateToAccountRoute = () => {
+    navigate(getRightPath());
+  };
 
-    return wallet === "Maiar" ? (
-        <li
-            style={getStyle()}
-            onClick={() => handleConnect("Maiar")}
-            className="wllListItem"
-            data-wallet="Maiar"
-        >
-            <img src={Maiar} alt="" />
-            <p>Maiar</p>
-        </li>
-    ) : (
-        <li
-            style={getStyle()}
-            onClick={() => handleConnect("Maiar Extension")}
-            className="wllListItem"
-            data-wallet="MaiarExtension"
-        >
-            <img src={Elrond} alt="Elrond Icon" />
-            <p>Maiar Extension</p>
-        </li>
-    );
+  const getStyle = () => {
+    if (temporaryFrom?.type === "Elrond") {
+      return {};
+    } else if (temporaryFrom && temporaryFrom?.type !== "Elrond") {
+      return OFF;
+    } else if (!from) {
+      return {};
+    } else if (from && from.text === "Elrond") {
+      return {};
+    } else return OFF;
+  };
+
+  return wallet === "Maiar" ? (
+    <li
+      style={getStyle()}
+      onClick={() => handleConnect("Maiar")}
+      className="wllListItem"
+      data-wallet="Maiar"
+    >
+      <img src={Maiar} alt="" />
+      <p>Maiar</p>
+    </li>
+  ) : (
+    <li
+      style={getStyle()}
+      onClick={() => handleConnect("Maiar Extension")}
+      className="wllListItem"
+      data-wallet="MaiarExtension"
+    >
+      <img src={Elrond} alt="Elrond Icon" />
+      <p>Maiar Extension</p>
+    </li>
+  );
 }
 ElrondWallet.propTypes = {
-    close: PropTypes.any,
-    wallet: PropTypes.string,
-    serviceContainer: PropTypes.object,
+  close: PropTypes.any,
+  wallet: PropTypes.string,
+  serviceContainer: PropTypes.object,
 };
 
 export default withServices(ElrondWallet);
