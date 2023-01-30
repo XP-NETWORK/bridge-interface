ae733e22669a5e2524e5f024ddf34ec83feec2c1
hassamullah <malikhassamullah1@yaho.com>
2023-01-30 12:05:51 +0500

    video NFTs pause button resolved, invalid recipient address inputs for EVM chains disabled

--- src/components/innercomponents/DestinationChain.jsx
+++ src/components/innercomponents/DestinationChain.jsx
@@ -3,56 +3,72 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import RedClose from "../../assets/img/icons/RedClose.svg";
 import {
-    setDepartureOrDestination,
-    setReceiver,
-    setSwitchDestination,
+	setDepartureOrDestination,
+	setReceiver,
+	setSwitchDestination,
+	setError,
 } from "../../store/reducers/generalSlice";
 import ChainSwitch from "../Buttons/ChainSwitch";
+import { isValidChecksumAddress } from "ethereumjs-util";
 
 function DestinationChain() {
-    const alert = useSelector((state) => state.general.pasteDestinationAlert);
-    const to = useSelector((state) => state.general.to);
+	const alert = useSelector((state) => state.general.pasteDestinationAlert);
+	const to = useSelector((state) => state.general.to);
 
-    const dispatch = useDispatch();
-    const receiver = useSelector((state) => state.general.receiver);
+	const dispatch = useDispatch();
+	const receiver = useSelector((state) => state.general.receiver);
 
-    const handleChange = (e) => {
-        dispatch(setReceiver(e.target.value));
-    };
+	const handleChange = (e) => {
+		try {
+			if (to.type === "EVM") {
+				const address = e.target.value;
+				if (address) {
+					const validAddress = isValidChecksumAddress(address);
+					if (validAddress) {
+						dispatch(setReceiver());
+					} else {
+						throw new Error(
+							"INVALID RECIPIENT ADDRESS: The recipient address is not a valid EVM address"
+						);
+					}
+				}
+			}
+		} catch (error) {
+			dispatch(setError(error));
+		}
+	};
 
-    function handleSwitchChain() {
-        dispatch(setDepartureOrDestination("destination"));
-        dispatch(setSwitchDestination(true));
-    }
+	function handleSwitchChain() {
+		dispatch(setDepartureOrDestination("destination"));
+		dispatch(setSwitchDestination(true));
+	}
 
-    useEffect(() => {}, [to]);
+	useEffect(() => {}, [to]);
 
-    return (
-        <div className="destination-props">
-            <div className="destination__header">
-                <span className="destination__title">Destination</span>
-                <ChainSwitch assignment={"to"} func={handleSwitchChain} />
-            </div>
+	return (
+		<div className="destination-props">
+			<div className="destination__header">
+				<span className="destination__title">Destination</span>
+				<ChainSwitch assignment={"to"} func={handleSwitchChain} />
+			</div>
 
-            <div
-                className={
-                    !alert
-                        ? "destination__address"
-                        : "destination__address desti-alert"
-                }
-            >
-                <input
-                    value={receiver}
-                    onChange={(e) => handleChange(e)}
-                    type="text"
-                    placeholder="Paste destination address"
-                />
-                <span className="invalid">
-                    <img src={RedClose} alt="Close" /> Invalid address
-                </span>
-            </div>
-        </div>
-    );
+			<div
+				className={
+					!alert ? "destination__address" : "destination__address desti-alert"
+				}
+			>
+				<input
+					value={receiver}
+					onChange={(e) => handleChange(e)}
+					type="text"
+					placeholder="Paste destination address"
+				/>
+				<span className="invalid">
+					<img src={RedClose} alt="Close" /> Invalid address
+				</span>
+			</div>
+		</div>
+	);
 }
 
 export default DestinationChain;
