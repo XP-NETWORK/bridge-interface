8fcabb42d255fc3ecdc07b7b4c75485d55f17126
YuraDXPNetwork <yurad@xp.network>
2023-02-06 17:37:55 +0200

    aptos solana near to testnet and comming chain false

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import * as generalSlice from "./store/reducers/generalSlice";
 import { useDispatch } from "react-redux";
 
 import {
-  checkValidators,
-  fetchXPUpdate,
-  transformToDate,
+    checkValidators,
+    fetchXPUpdate,
+    transformToDate,
 } from "./wallet/helpers";
 import { chains } from "./components/values";
 
@@ -19,67 +19,58 @@ import Modals from "./components/Modals/Modals";
 import AppContainer from "./components/App/container";
 
 function App({ network }) {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    localStorage.removeItem("walletconnect");
-    dispatch(generalSlice.setInnerWidth(window.innerWidth));
-    const from = new URLSearchParams(window.location.search).get("from");
-    const to = new URLSearchParams(window.location.search).get("to");
+    useEffect(() => {
+        localStorage.removeItem("walletconnect");
+        dispatch(generalSlice.setInnerWidth(window.innerWidth));
+        const from = new URLSearchParams(window.location.search).get("from");
+        const to = new URLSearchParams(window.location.search).get("to");
 
-    if (from !== to) {
-      if (from) {
-        const fromChain = chains.filter(
-          (n) => n.text === from.replace("/", "")
-        )[0];
-        if (fromChain) {
-          dispatch(generalSlice.setFrom(fromChain));
+        if (from !== to) {
+            if (from) {
+                const fromChain = chains.filter(
+                    (n) => n.text === from.replace("/", "")
+                )[0];
+                if (fromChain) {
+                    dispatch(generalSlice.setFrom(fromChain));
+                }
+            }
+            if (to) {
+                const toChain = chains.filter(
+                    (n) => n.text === to.replace("/", "")
+                )[0];
+                if (toChain) {
+                    dispatch(generalSlice.setTo(toChain));
+                }
+            }
         }
-      }
-      if (to) {
-        const toChain = chains.filter((n) => n.text === to.replace("/", ""))[0];
-        if (toChain) {
-          dispatch(generalSlice.setTo(toChain));
-        }
-      }
-    }
-    fetchXPUpdate().then((data) => {
-      const d = transformToDate(data);
-      dispatch(generalSlice.setGitLatestCommit(d));
-    });
-    checkValidators().then((data) => {
-      dispatch(generalSlice.setValidatorsInf(data));
-    });
-    const validatorsInt = setInterval(() => {
-      checkValidators().then((data) => {
-        dispatch(generalSlice.setValidatorsInf(data));
-      });
-    }, 10000);
+        fetchXPUpdate().then((data) => {
+            const d = transformToDate(data);
+            dispatch(generalSlice.setGitLatestCommit(d));
+        });
+        checkValidators().then((data) => {
+            dispatch(generalSlice.setValidatorsInf(data));
+        });
+        const validatorsInt = setInterval(() => {
+            checkValidators().then((data) => {
+                dispatch(generalSlice.setValidatorsInf(data));
+            });
+        }, 10000);
 
-    /*const tweb = new TonWeb(
-      new TonWeb.HttpProvider("https://toncenter.com/api/v2/jsonRPC", {
-        apiKey:
-          "05645d6b549f33bf80cee8822bd63df720c6781bd00020646deb7b2b2cd53b73",
-      })
-    )
+        return () => clearInterval(validatorsInt);
+    }, []);
 
-     tweb.provider.getTransactions('EQBABLUFRe95jzxV8E_XzTsLtK-3eggjs5eVXviA4VLY0UMW', 20).then(trxs => {
-      console.log(trxs, 'trxs')
-    })*/
-
-    return () => clearInterval(validatorsInt);
-  }, []);
-
-  return (
-    <div className={"App"}>
-      <AppContainer>
-        <Modals />
-        <XpBridge network={network} />
-        <Alert />
-        <DepositAlert />
-      </AppContainer>
-    </div>
-  );
+    return (
+        <div className={"App"}>
+            <AppContainer>
+                <Modals />
+                <XpBridge network={network} />
+                <Alert />
+                <DepositAlert />
+            </AppContainer>
+        </div>
+    );
 }
 
 export default App;
