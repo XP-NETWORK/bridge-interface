719dbf2f66995bfe5d1931b4b967937ab2d1ca7b
YuraDXPNetwork <yurad@xp.network>
2023-02-09 12:48:49 +0200

    copy text to clipboard in account modal fixed

--- src/components/Modals/AccountModal/AccountModal.jsx
+++ src/components/Modals/AccountModal/AccountModal.jsx
@@ -3,7 +3,7 @@ import NftSelect from "../../../assets/img/nftselect.svg";
 
 import { useSelector, useDispatch } from "react-redux";
 import { setAccountModal } from "../../../store/reducers/generalSlice";
-import { CopyToClipboard } from "react-copy-to-clipboard";
+// import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import Tooltip from "./Tooltip";
 import { DetectOutsideClick } from "./accountModalHelper";
@@ -17,6 +17,14 @@ export default function AccountModal() {
     );
 
     const show = useSelector((state) => state.general.accountModal);
+
+    const copyTextToClipboard = async () => {
+        if ("clipboard" in navigator) {
+            return await navigator.clipboard.writeText(account);
+        } else {
+            return document.execCommand("copy", true, account);
+        }
+    };
 
     const handleClose = () => {
         dispatch(setAccountModal(false));
@@ -43,16 +51,19 @@ export default function AccountModal() {
             </div>
             <p>{connectedWallet}</p>
 
-            <CopyToClipboard text={account}>
-                <div className="account-modal__account">
-                    <img src={NftSelect} alt="#" />
-                    {account &&
-                        `${account.substring(0, 10)}...${account.substring(
-                            account.length - 2
-                        )}`}
-                    <Tooltip />
-                </div>
-            </CopyToClipboard>
+            {/* <CopyToClipboard text={account}> */}
+            <div
+                onClick={copyTextToClipboard}
+                className="account-modal__account"
+            >
+                <img src={NftSelect} alt="#" />
+                {account &&
+                    `${account.substring(0, 10)}...${account.substring(
+                        account.length - 2
+                    )}`}
+                <Tooltip />
+            </div>
+            {/* </CopyToClipboard> */}
             <div className="accountBtn">
                 <button
                     onClick={() => window.location.reload()}
