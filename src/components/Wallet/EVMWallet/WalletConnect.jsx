e76d2c808d75803a45b30bcde4ce910e79dfa788
YuraDXPNetwork <yurad@xp.network>
2023-02-28 15:14:19 +0200

    xp.network upgraded

--- src/components/Wallet/EVMWallet/WalletConnect.jsx
+++ src/components/Wallet/EVMWallet/WalletConnect.jsx
@@ -5,46 +5,47 @@ import HigherEVM from "./HigherEVM";
 import { useDispatch, useSelector } from "react-redux";
 import { useWeb3Modal } from "@web3modal/react";
 import {
-  setAccountWalletModal,
-  setWalletsModal,
+    setAccountWalletModal,
+    setWalletsModal,
 } from "../../../store/reducers/generalSlice";
 import { wcSupportedChains } from "./evmConnectors";
 
 function WalletConnect({ styles /*connectWallet*/ }) {
-  const dispatch = useDispatch();
-  const { open } = useWeb3Modal();
-  const from = useSelector((state) => state.general.from);
+    const dispatch = useDispatch();
+    const { open } = useWeb3Modal();
+    const from = useSelector((state) => state.general.from);
+    const testnet = useSelector((state) => state.general.testNet);
 
-  const handleClick = async () => {
-    dispatch(setWalletsModal(false));
-    dispatch(setAccountWalletModal(false));
-    await open();
-  };
+    const handleClick = async () => {
+        dispatch(setWalletsModal(false));
+        dispatch(setAccountWalletModal(false));
+        await open();
+    };
 
-  const isSupported = wcSupportedChains.find(
-    (supported) =>
-      from?.chainId === supported.id || from?.tnChainId === supported.id
-  );
+    const isSupported = wcSupportedChains.find(
+        (supported) =>
+            from?.chainId === supported.id || from?.tnChainId === supported.id
+    );
 
-  return (
-    <li
-      style={
-        from && isSupported
-          ? styles()
-          : { pointerEvents: "none", opacity: "0.7" }
-      }
-      // onClick={() => connectWallet("WalletConnect")}
-      onClick={handleClick}
-      className="wllListItem"
-      data-wallet="WalletConnect"
-    >
-      <img src={icon} alt="WalletConnect Icon" />
-      <p>WalletConnect</p>
-    </li>
-  );
+    return (
+        <li
+            style={
+                from && isSupported && !testnet
+                    ? styles()
+                    : { pointerEvents: "none", opacity: "0.7" }
+            }
+            // onClick={() => connectWallet("WalletConnect")}
+            onClick={handleClick}
+            className="wllListItem"
+            data-wallet="WalletConnect"
+        >
+            <img src={icon} alt="WalletConnect Icon" />
+            <p>WalletConnect</p>
+        </li>
+    );
 }
 WalletConnect.propTypes = {
-  styles: PropTypes.func,
-  connectWallet: PropTypes.func,
+    styles: PropTypes.func,
+    connectWallet: PropTypes.func,
 };
 export default HigherEVM(WalletConnect);
