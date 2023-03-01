f737023e83a3218b53549162fc77118cb364fd4c
Hassam Ullah <malikhassamullah1@yahoo.com>
2023-03-01 17:31:00 +0500

    address input bug fixed

--- src/components/innercomponents/DestinationChain.jsx
+++ src/components/innercomponents/DestinationChain.jsx
@@ -166,6 +166,7 @@ function DestinationChain() {
                 }
 
                 default: {
+                  dispatch(setReceiver(address));
                   break;
                 }
               }
