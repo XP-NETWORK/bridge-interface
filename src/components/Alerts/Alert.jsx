71b026ba083964c8721f62a0d2df9dd6883b08af
Bilal01230 <bilalahmad01230@gmail.com>
2023-03-27 13:38:28 +0500

    Desitination and Departure chain selection modal fix + testnet connectivity on mainnet(in progress)

--- src/components/Alerts/Alert.jsx
+++ src/components/Alerts/Alert.jsx
@@ -4,30 +4,51 @@ import { ReactComponent as Close } from "../../assets/img/icons/close.svg";
 import { setAlert } from "../../store/reducers/generalSlice";
 
 function Alert() {
-    const dispatch = useDispatch();
-    const to = useSelector((state) => state.general.to);
-    const from = useSelector((state) => state.general.from);
-    const alert = useSelector((state) => state.general.alert);
+  const dispatch = useDispatch();
+  const to = useSelector((state) => state.general.to);
+  const from = useSelector((state) => state.general.from);
+  const alert = useSelector((state) => state.general.alert);
 
-    const handleClose = () => {
-        dispatch(setAlert(false));
-    };
-
-    return (
-        <div id="alertb">
-            {(from && to) || !alert ? (
-                ""
-            ) : (
-                <div className="aleartBox">
-                    Select Departure and Destination Chain to continue bridging
-                    <span onClick={() => handleClose()} className="closeBox">
-                        {" "}
-                        <Close className="svgWidget closeIcon" />
-                    </span>
-                </div>
-            )}
+  const handleClose = () => {
+    dispatch(setAlert(false));
+  };
+  return (
+    <div id="alertb">
+      {(from && to === undefined) || !alert ? (
+        ""
+      ) : (
+        <div className="aleartBox">
+          Select Departure Chain to continue bridging
+          <span onClick={() => handleClose()} className="closeBox">
+            {" "}
+            <Close className="svgWidget closeIcon" />
+          </span>
         </div>
-    );
+      )}
+      {(from === undefined && to) || !alert ? (
+        ""
+      ) : (
+        <div className="aleartBox">
+          Select Destination Chain to continue bridging
+          <span onClick={() => handleClose()} className="closeBox">
+            {" "}
+            <Close className="svgWidget closeIcon" />
+          </span>
+        </div>
+      )}
+      {(from && to) === undefined || !alert ? (
+        ""
+      ) : (
+        <div className="aleartBox">
+          Select Departure and Destination Chain to continue bridging
+          <span onClick={() => handleClose()} className="closeBox">
+            {" "}
+            <Close className="svgWidget closeIcon" />
+          </span>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Alert;
