8c6ab8a34321fd8205279e206d69d8eef7da5121
YuraDXPNetwork <yurad@xp.network>
2023-03-29 12:31:01 +0300

    audit logo added

--- src/components/innercomponents/AuditedBy.jsx
+++ src/components/innercomponents/AuditedBy.jsx
@@ -3,7 +3,7 @@ import certik from "../../assets/img/audited_by/certik.svg";
 import hacken from "../../assets/img/audited_by/hacken.svg";
 import smart_state from "../../assets/img/audited_by/smart_state.svg";
 import safepress from "../../assets/img/audited_by/safepress.svg";
-// import blocksec from "../../assets/img/audited_by/blocksec.png";
+import blocksec from "../../assets/img/audited_by/blocksec.svg";
 
 import "./AuditedBy.css";
 export default function AuditedBy() {
@@ -16,7 +16,7 @@ export default function AuditedBy() {
                     <img src={hacken} alt="Hacken" />
                     <img src={smart_state} alt="Smart state" />
                     <img src={safepress} alt="Safepress" />
-                    {/* <img src={blocksec} alt="Blocksec" /> */}
+                    <img src={blocksec} alt="Blocksec" />
                 </div>
             </div>
         </div>
