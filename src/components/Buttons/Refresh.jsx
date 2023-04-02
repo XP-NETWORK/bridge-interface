71e7c2dff93fb11f9027db3ade4b2559480c493a
YuraDXPNetwork <yurad@xp.network>
2023-04-02 09:57:15 +0300

    added event for analytics in refresh nfts button

--- src/components/Buttons/Refresh.jsx
+++ src/components/Buttons/Refresh.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { googleAnalyticsCategories, handleGA4Event } from "../../services/GA4";
 import {
-  setBigLoader,
-  setNFTList,
-  setPreloadNFTs,
-  setError,
+    setBigLoader,
+    setNFTList,
+    setPreloadNFTs,
+    setError,
 } from "../../store/reducers/generalSlice";
 
 import { setIsEmpty } from "../../store/reducers/paginationSlice";
@@ -12,50 +13,58 @@ import { setIsEmpty } from "../../store/reducers/paginationSlice";
 import { withServices } from "../App/hocs/withServices";
 
 export default withServices(function Refresh({ serviceContainer }) {
-  const {
-    from,
-    nfts,
-    account,
-    bigLoader,
+    const {
+        from,
+        nfts,
+        account,
+        bigLoader,
 
-    secretCred,
-  } = useSelector((state) => state.general);
-  const dispatch = useDispatch();
+        secretCred,
+    } = useSelector((state) => state.general);
+    const dispatch = useDispatch();
 
-  const { bridge } = serviceContainer;
+    const { bridge } = serviceContainer;
 
-  const refresh = async () => {
-    if (!bigLoader || !nfts) {
-      let w = account;
+    const refresh = async () => {
+        handleGA4Event(googleAnalyticsCategories.Button, "Refresh nfts");
+        if (!bigLoader || !nfts) {
+            let w = account;
 
-      const fromChain = await bridge.getChain(from.nonce);
-      dispatch(setBigLoader(true));
-      try {
-        let nfts = await fromChain.getNFTs(bridge.checkWallet || w, secretCred);
-        nfts = fromChain.filterNFTs(nfts);
+            const fromChain = await bridge.getChain(from.nonce);
+            dispatch(setBigLoader(true));
+            try {
+                let nfts = await fromChain.getNFTs(
+                    bridge.checkWallet || w,
+                    secretCred
+                );
+                nfts = fromChain.filterNFTs(nfts);
 
-        if (nfts.length < 1) {
-          dispatch(setIsEmpty(true));
-        } else {
-          dispatch(setIsEmpty(false));
+                if (nfts.length < 1) {
+                    dispatch(setIsEmpty(true));
+                } else {
+                    dispatch(setIsEmpty(false));
+                }
+
+                dispatch(setPreloadNFTs(nfts.length));
+                dispatch(setNFTList(nfts));
+                dispatch(setBigLoader(false));
+            } catch (error) {
+                console.log(error);
+                dispatch(setBigLoader(false));
+                dispatch(setNFTList([]));
+                dispatch(
+                    setError(error.data ? error.data.message : error.message)
+                );
+            }
         }
+    };
 
-        dispatch(setPreloadNFTs(nfts.length));
-        dispatch(setNFTList(nfts));
-        dispatch(setBigLoader(false));
-      } catch (error) {
-        console.log(error);
-        dispatch(setBigLoader(false));
-        dispatch(setNFTList([]));
-        dispatch(setError(error.data ? error.data.message : error.message));
-      }
-    }
-  };
-
-  return (
-    <span
-      className={bigLoader ? "refresh-button--disabled" : "refresh-button"}
-      onClick={refresh}
-    ></span>
-  );
+    return (
+        <span
+            className={
+                bigLoader ? "refresh-button--disabled" : "refresh-button"
+            }
+            onClick={refresh}
+        ></span>
+    );
 });
