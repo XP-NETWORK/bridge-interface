44b00536290b541656a0a512ca1d9fc7854fdc8f
Hassam Ullah <malikhassamullah1@yahoo.com>
2023-04-20 13:39:44 +0500

    near wallet filter bug fix

--- src/components/Wallet/SOLWallet/Phantom.jsx
+++ src/components/Wallet/SOLWallet/Phantom.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 import icon from "../../../assets/img/wallet/phantom-icon-purple.svg";
-
+import { useSelector } from "react-redux";
 import HigherSolana from "./HigherSolana";
 
-function Phantom({ connectWallet, styles }) {
-    return (
-        <li
-            style={styles()}
-            onClick={() => connectWallet("Phantom")}
-            className="wllListItem"
-            data-wallet="Phantom"
-        >
-            <img style={{ width: "28px" }} src={icon} alt="phantomWallet" />
-            <p>Phantom</p>
-        </li>
-    );
+function Phantom({ connectWallet }) {
+  const from = useSelector((state) => state.general.from);
+  const OFF = { opacity: 0.7, pointerEvents: "none" };
+  const getStyle = () => {
+    if (from) {
+      if (from?.type !== "Solana") {
+        return OFF;
+      }
+    } else {
+      return OFF;
+    }
+  };
+  return (
+    <li
+      style={getStyle()}
+      onClick={() => connectWallet("Phantom")}
+      className="wllListItem"
+      data-wallet="Phantom"
+    >
+      <img style={{ width: "28px" }} src={icon} alt="phantomWallet" />
+      <p>Phantom</p>
+    </li>
+  );
 }
 Phantom.propTypes = {
-    styles: PropTypes.func,
-    connectWallet: PropTypes.func,
-    close: PropTypes.func,
+  styles: PropTypes.func,
+  connectWallet: PropTypes.func,
+  close: PropTypes.func,
 };
 export default HigherSolana(Phantom);
