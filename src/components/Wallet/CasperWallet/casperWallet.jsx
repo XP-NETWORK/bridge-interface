9350defc16745244a047163483e024647e906a28
D <ddisdead462@gmail.com>
2024-05-15 16:35:24 +0500

    casper fixes

--- src/components/Wallet/CasperWallet/casperWallet.jsx
+++ src/components/Wallet/CasperWallet/casperWallet.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -10,66 +11,141 @@ import { getRightPath } from "../../../utils";
 import { withServices } from "../../App/hocs/withServices";
 
 import { Chain } from "xp.network";
-import { setAccount, setError, setFrom } from "../../../store/reducers/generalSlice";
+import {
+  setAccount,
+  setError,
+  setFrom,
+} from "../../../store/reducers/generalSlice";
 
 import casperIcon from "../../../assets/img/wallet/casper.svg";
 
 //import { googleAnalyticsCategories, handleGA4Event } from "../../services/GA4";
 
 function CasperWallet({ serviceContainer }) {
-    const { bridge } = serviceContainer;
-    const isMobile = useCheckMobileScreen();
-    const from = useSelector((state) => state.general.from);
-    const to = useSelector((state) => state.general.to);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const navigateToAccountRoute = () => {
-        navigate(getRightPath(bridge.network));
-    };
+  const { bridge } = serviceContainer;
+  const isMobile = useCheckMobileScreen();
+  const from = useSelector((state) => state.general.from);
+  const to = useSelector((state) => state.general.to);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const navigateToAccountRoute = () => {
+    navigate(getRightPath(bridge.network));
+  };
 
-    const onClickHandler = async () => {
-        //lockBtn(true);
+  const getCasperWalletSession = async () => {
+    //update your app content for the new session
 
-        const { CasperWalletProvider } = window;
-        const account = {};
+    const CasperWalletProvider = await window.csprclick.getProviderInstance(
+      "casper-wallet"
+    );
+    const account = {};
 
-        if (!CasperWalletProvider) {
-            return dispatch(
-                setError({
-                    link: {
-                        href:
-                            "https://chrome.google.com/webstore/detail/casper-wallet/abkahkcbhngaebpcgfmhkoioedceoigp",
-                        text: "here",
-                    },
-                    message: "Install Casper Wallet",
-                })
-            );
-        }
+    // if (!CasperWalletProvider) {
+    //   return dispatch(
+    //     setError({
+    //       link: {
+    //         href:
+    //           "https://chrome.google.com/webstore/detail/casper-wallet/abkahkcbhngaebpcgfmhkoioedceoigp",
+    //         text: "here",
+    //       },
+    //       message: "Install Casper Wallet",
+    //     })
+    //   );
+    // }
 
-        const [chainWrapper] = await Promise.all([bridge.getChain(Chain.CASPER)]);
+    const [chainWrapper] = await Promise.all([bridge.getChain(Chain.CASPER)]);
 
-        const provider = CasperWalletProvider();
+    const provider = CasperWalletProvider.provider;
 
-        const connection = await provider.requestConnection(); //boolean
+    // const connection = await provider.requestConnection(); //boolean
 
-        if (!connection) return dispatch(setError({ message: "Could not establish a connection" }));
+    // if (!connection)
+    //   return dispatch(
+    //     setError({ message: "Could not establish a connection" })
+    //   );
 
-        account.address = await provider.getActivePublicKey();
-        account.signer = provider;
-        chainWrapper.chain.setProxy(proxy);
+    account.address = await provider.getActivePublicKey();
+    account.signer = provider;
+    chainWrapper.chain.setProxy(proxy);
 
-        chainWrapper.setSigner(account.signer);
-        bridge.setCurrentType(chainWrapper);
+    chainWrapper.setSigner(account.signer);
+    bridge.setCurrentType(chainWrapper);
 
-        false &&
-            (await chainWrapper.chain.mintNft(account.signer, {
-                contract: "hash-23ecf377ab0de596cbda5b6e1cdfd230bad2f8eee688d7b5902bd560ffd96b4e",
-                uri: `https://meta.polkamon.com/meta?id=100023665${Math.floor(Math.random() * 10)}${Math.floor(
-                    Math.random() * 10
-                )}`,
-            }));
+    false &&
+      (await chainWrapper.chain.mintNft(account.signer, {
+        contract:
+          "hash-23ecf377ab0de596cbda5b6e1cdfd230bad2f8eee688d7b5902bd560ffd96b4e",
+        uri: `https://meta.polkamon.com/meta?id=100023665${Math.floor(
+          Math.random() * 10
+        )}${Math.floor(Math.random() * 10)}`,
+      }));
 
-        /* console.log(await chainWrapper.chain.validateAddress(account.address));
+    dispatch(setAccount(account.address));
+
+    if (!from) dispatch(setFrom(getChainObject(Chain.CASPER)));
+
+    if (from && to) navigateToAccountRoute();
+  };
+  window.csprclick.on("csprclick:signed_in", async (evt) => {
+    console.log("signed_in", evt);
+    await getCasperWalletSession();
+  });
+  window.csprclick.on("csprclick:switched_account", async (evt) => {
+    console.log("switched_account", evt);
+    await getCasperWalletSession();
+  });
+  window.csprclick.on("csprclick:unsolicited_account_change", async (evt) => {
+    console.log("switched_account", evt);
+    await getCasperWalletSession();
+  });
+
+  const onClickHandler = async () => {
+    //lockBtn(true);
+
+    const { CasperWalletProvider } = window;
+    const account = {};
+
+    if (!CasperWalletProvider) {
+      return dispatch(
+        setError({
+          link: {
+            href:
+              "https://chrome.google.com/webstore/detail/casper-wallet/abkahkcbhngaebpcgfmhkoioedceoigp",
+            text: "here",
+          },
+          message: "Install Casper Wallet",
+        })
+      );
+    }
+
+    const [chainWrapper] = await Promise.all([bridge.getChain(Chain.CASPER)]);
+
+    const provider = CasperWalletProvider();
+
+    const connection = await provider.requestConnection(); //boolean
+
+    if (!connection)
+      return dispatch(
+        setError({ message: "Could not establish a connection" })
+      );
+
+    account.address = await provider.getActivePublicKey();
+    account.signer = provider;
+    chainWrapper.chain.setProxy(proxy);
+
+    chainWrapper.setSigner(account.signer);
+    bridge.setCurrentType(chainWrapper);
+
+    false &&
+      (await chainWrapper.chain.mintNft(account.signer, {
+        contract:
+          "hash-23ecf377ab0de596cbda5b6e1cdfd230bad2f8eee688d7b5902bd560ffd96b4e",
+        uri: `https://meta.polkamon.com/meta?id=100023665${Math.floor(
+          Math.random() * 10
+        )}${Math.floor(Math.random() * 10)}`,
+      }));
+
+    /* console.log(await chainWrapper.chain.validateAddress(account.address));
 
         console.log(
             await chainWrapper.chain.validateAddress(
@@ -84,30 +160,27 @@ function CasperWallet({ serviceContainer }) {
             "d"
         );*/
 
-        dispatch(setAccount(account.address));
+    dispatch(setAccount(account.address));
 
-        if (!from) dispatch(setFrom(getChainObject(Chain.CASPER)));
+    if (!from) dispatch(setFrom(getChainObject(Chain.CASPER)));
 
-        if (from && to) navigateToAccountRoute();
-    };
+    if (from && to) navigateToAccountRoute();
+  };
 
-    const getStyle = () => {
-        return {};
-    };
-
-    return (
-        <>
-            <li
-                style={isMobile ? { display: "none" } : getStyle()}
-                onClick={() => onClickHandler("CasperWallet")}
-                className="wllListItem keplr"
-                data-wallet="CasperWallet"
-            >
-                <img src={casperIcon} alt="CasperWallet" />
-                <p>Casper Wallet</p>
-            </li>
-        </>
-    );
+  return (
+    <>
+      <li
+        onClick={async () => {
+          await window.csprclick.connect("casper-wallet");
+        }}
+        className="wllListItem keplr"
+        data-wallet="CasperWallet"
+      >
+        <img src={casperIcon} alt="CasperWallet" />
+        <p>Casper Wallet</p>
+      </li>
+    </>
+  );
 }
 
 export default withServices(CasperWallet);
