8afd8d52bf2ade3d45fe4ed3786cd53c13ea8ca1
Muhammad Usman <muhammadusman28510@gmail.com>
2024-10-08 16:19:07 +0500

    fix(near): fix nft listing issues. add sleep before read claim event

--- src/components/Buttons/Refresh.jsx
+++ src/components/Buttons/Refresh.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { googleAnalyticsCategories, handleGA4Event } from "../../services/GA4";
 import {
-    setBigLoader,
-    setNFTList,
-    setPreloadNFTs,
-    setError,
+  setBigLoader,
+  setNFTList,
+  setPreloadNFTs,
+  setError,
 } from "../../store/reducers/generalSlice";
 
 import { setIsEmpty } from "../../store/reducers/paginationSlice";
@@ -13,58 +13,57 @@ import { setIsEmpty } from "../../store/reducers/paginationSlice";
 import { withServices } from "../App/hocs/withServices";
 
 export default withServices(function Refresh({ serviceContainer }) {
-    const {
-        from,
-        nfts,
-        account,
-        bigLoader,
-        preFetchData,
-        secretCred,
-    } = useSelector((state) => state.general);
-    const dispatch = useDispatch();
+  const {
+    from,
+    nfts,
+    account,
+    bigLoader,
+    preFetchData,
+    secretCred,
+  } = useSelector((state) => state.general);
+  const dispatch = useDispatch();
 
-    const { bridge } = serviceContainer;
+  const { bridge } = serviceContainer;
 
-    const refresh = async () => {
-        handleGA4Event(googleAnalyticsCategories.Button, "Refresh nfts");
-        if (!bigLoader || !nfts) {
-            let w = account;
+  const refresh = async () => {
+    handleGA4Event(googleAnalyticsCategories.Button, "Refresh nfts");
+    if (!bigLoader || !nfts) {
+      let w = account;
 
-            const fromChain = await bridge.getChain(from.nonce);
-            dispatch(setBigLoader(true));
-            try {
-                let nfts = await fromChain.getNFTs(
-                    bridge.checkWallet || w,
-                    secretCred.contract || preFetchData?.contract
-                );
-                nfts = fromChain.filterNFTs(nfts);
+      const fromChain = await bridge.getChain(from.nonce);
+      dispatch(setBigLoader(true));
+      try {
+        let nfts = await fromChain.getNFTs(
+          bridge.checkWallet || w,
+          secretCred.contract || preFetchData?.contract,
+          {
+            viewingKey: secretCred?.viewKey,
+          },
+        );
+        nfts = fromChain.filterNFTs(nfts);
 
-                if (nfts.length < 1) {
-                    dispatch(setIsEmpty(true));
-                } else {
-                    dispatch(setIsEmpty(false));
-                }
-
-                dispatch(setPreloadNFTs(nfts.length));
-                dispatch(setNFTList(nfts));
-                dispatch(setBigLoader(false));
-            } catch (error) {
-                console.log(error);
-                dispatch(setBigLoader(false));
-                dispatch(setNFTList([]));
-                dispatch(
-                    setError(error.data ? error.data.message : error.message)
-                );
-            }
+        if (nfts.length < 1) {
+          dispatch(setIsEmpty(true));
+        } else {
+          dispatch(setIsEmpty(false));
         }
-    };
 
-    return (
-        <span
-            className={
-                bigLoader ? "refresh-button--disabled" : "refresh-button"
-            }
-            onClick={refresh}
-        ></span>
-    );
+        dispatch(setPreloadNFTs(nfts.length));
+        dispatch(setNFTList(nfts));
+        dispatch(setBigLoader(false));
+      } catch (error) {
+        console.log(error);
+        dispatch(setBigLoader(false));
+        dispatch(setNFTList([]));
+        dispatch(setError(error.data ? error.data.message : error.message));
+      }
+    }
+  };
+
+  return (
+    <span
+      className={bigLoader ? "refresh-button--disabled" : "refresh-button"}
+      onClick={refresh}
+    ></span>
+  );
 });
