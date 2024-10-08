aa8b808aaee15c2f1c4c12d1835b63ec3628a0c6
Muhammad Usman <muhammadusman28510@gmail.com>
2024-10-08 16:55:10 +0500

    Revert "chore(scrt): merge mx branch"
    This reverts commit 8c063bb44189cb98149ee78adfa1c02f6fcd4ad7.

--- src/components/Modals/Success/TransferredNft.jsx
+++ src/components/Modals/Success/TransferredNft.jsx
@@ -164,8 +164,8 @@ const TransferredNft = ({
       from.type === "DFINITY"
     ) {
       setTxnStatus("completed");
-    } else if (txn?.provider && txn?.hash) {
-      evmTxStatus(txn?.provider, txn?.hash)
+    } else {
+      evmTxStatus(txn.provider, txn.hash)
         .then((res) => {
           if (res) {
             setTxnStatus("completed");
