d26a9a6076e55773bf3123ade0dc3b377f1e0aaf
Muhammad Usman <muhammadusman28510@gmail.com>
2024-10-11 10:53:37 +0500

    fix: add MULTIVERSX to chain identifier check

--- src/utils/xpDecentralizedUtility.js
+++ src/utils/xpDecentralizedUtility.js
@@ -352,7 +352,7 @@ export class XPDecentralizedUtility {
       v3_ChainId[targetChainIdentifier?.nonce].name === "HEDERA" ||
       v3_ChainId[targetChainIdentifier?.nonce].name === "ICP" ||
       v3_ChainId[targetChainIdentifier?.nonce].name === "TEZOS" ||
-      v3_ChainId[targetChainIdentifier?.nonce].name === "SECRET"
+      v3_ChainId[targetChainIdentifier?.nonce].name === "SECRET" ||
       v3_ChainId[targetChainIdentifier?.nonce].name === "MULTIVERSX"
     ) {
       return {
